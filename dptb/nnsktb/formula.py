1a0c0207abd528415e5bf5d1380e7d977546b675
qqgu <guqq@pku.edu.cn>
2022-08-10 02:05:46 +0000

    creat a frame for nn sktb fitting

---
+++ dptb/nnsktb/formula.py
@@ -0,0 +1,5 @@
+# define the integrals formula.
+
+
+def skformula(alpha1 ,alpha2, alpha3, alpha4, **kwargs):
+    pass
---
+++ dptb/nnsktb/onsiteFunc.py
@@ -0,0 +1,1 @@
+# define the function for output all the onsites Es for given i.
