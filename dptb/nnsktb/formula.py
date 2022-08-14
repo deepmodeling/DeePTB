e9b3a518fed46feec8d91d5f6a75a62d22735c82
qqgu <guqq@pku.edu.cn>
2022-08-15 02:29:05 +0800

    update formula

--- dptb/nnsktb/formula.py
+++ dptb/nnsktb/formula.py
@@ -23,12 +23,13 @@ class SKFormula(BaseSK):
     def __init__(self,mode='varTang96') -> None: 
         # one can modify this by add his own formula with the name mode to deifine num of pars.
         if mode == 'varTang96':
+            self.mode = mode
             self.num_paras = 4
         else:
             raise ValueError('No such formula')
         
 
-    def skhij(self, mode='varTang96', **kwargs):
+    def skhij(self, **kwargs):
         '''This is a wrap function for a self-defined formula of sk integrals. one can easily modify it into whatever form they want.
         
         Returns
@@ -37,22 +38,22 @@ class SKFormula(BaseSK):
         
         '''
 
-        if mode == 'varTang96':
+        if self.mode == 'varTang96':
             return self.varTang96(**kwargs)
         else:
             raise ValueError('No such formula')
 
 
     def varTang96(self, paraArray, rij, **kwargs):
-        # sourcery skip: inline-immediately-returned-variable
-        '''> This function calculates the value of the variational form of Tang et al 1996. without the
+        """> This function calculates the value of the variational form of Tang et al 1996. without the
         environment dependent
 
                 $$ h(rij) = \alpha_1 * (rij)^(-\alpha_2) * exp(-\alpha_3 * (rij)^(\alpha_4))$$
-        '''
-        assert len(paraArray) >= self.num_paras, 'Not enough parameters'
-        alpha1, alpha2, alpha3, alpha4 = paraArray[0:4]
-        hij = alpha1 * (rij**(-alpha2)) * th.exp(-alpha3 * (rij**alpha4))
+        """
 
-        return hij    
+        assert len(paraArray.shape) in {2, 1}, 'paraArray should be a 2d tensor or 1d tensor'
+        paraArray = paraArray.view(-1, self.num_paras)
+        alpha1, alpha2, alpha3, alpha4 = paraArray[:, 0], paraArray[:, 1], paraArray[:, 2], paraArray[:, 3]
+
+        return alpha1 * rij**(-alpha2) * th.exp(-alpha3 * rij**alpha4)    
 
