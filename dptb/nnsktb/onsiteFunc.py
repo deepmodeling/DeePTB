86ab946080e69d2fcc21a4e0a0831ebe9d5e0b10
qqgu <guqq@pku.edu.cn>
2022-09-02 01:57:05 +0800

    ✨ feat(nnsktb.onsiteFunc, structure.structure, utils.tools): add support for individual OnsiteEnergy for sub-orb like px...
    1. add the Onsite_Ind_Mapings_OrbSplit function in Index_Mapings class in tools.py
    
    2. add a  onsitemode input for BaseStruct class in choose the onsite Energy type.
    
    3.  modify the onsiteFunc to make it general for both onsite Energy mapping type.

--- dptb/nnsktb/onsiteFunc.py
+++ dptb/nnsktb/onsiteFunc.py
@@ -10,8 +10,16 @@ def loadOnsite(onsite_map: dict):
 
     Parameters:
     -----------
-        onsite_map: dict
-            for example: {'N': {'2s': [0], '2p': [1]}, 'B': {'2s': [0], '2p': [1]}}
+        onsite_map: dict, has two possible format.
+            -1. {'N': {'2s': [0], '2p': [1]}, 'B': {'2s': [0], '2p': [1]}}
+            -2. {'N': {'2s': [0], '2p': [1,2,3]}, 'B': {'2s': [0], '2p': [1,2,3]}}
+    
+    Returns:
+    --------
+        onsite energy: dict, the format follows the input onsite_map, e.g.:
+            -1. {'N':tensor[es,ep], 'B': tensor[es,ep]}
+            -2. {'N':tensor[es,ep1,ep2,ep3], 'B': tensor[es,ep1,ep2,ep3]}
+
     """
 
     atoms_types = list(onsite_map.keys())
@@ -19,7 +27,8 @@ def loadOnsite(onsite_map: dict):
     for ia in atoms_types:
         assert ia in onsite_energy_database.keys(), f'{ia} is not in the onsite_energy_database. \n see the onsite_energy_database in dptb.nnsktb.onsiteDB.py.'
         orb_energies = onsite_energy_database[ia]
-        onsite_db[ia] = th.zeros(len(onsite_map[ia]))
+        indeces = sum(list(onsite_map[ia].values()),[])
+        onsite_db[ia] = th.zeros(len(indeces))
         for isk in onsite_map[ia].keys():
             assert isk in orb_energies.keys(), f'{isk} is not in the onsite_energy_database for {ia} atom. \n see the onsite_energy_database in dptb.nnsktb.onsiteDB.py.'
             onsite_db[ia][onsite_map[ia][isk]] = orb_energies[isk]
@@ -35,12 +44,13 @@ def onsiteFunc(batch_bonds_onsite, onsite_db):
             e.g.:  dict(f: [[f, 7, 0, 7, 0, 0, 0, 0],
                              [f, 5, 1, 5, 1, 0, 0, 0]])
         onsite_db: dict from function loadOnsite
-            e.g.: {'N':tensor[es,ep], 'B': tensor[es,ep]}
+            e.g.: {'N':tensor[es,ep], 'B': tensor[es,ep]} or {'N':tensor[es,ep1,ep2,ep3], 'B': tensor[es,ep1,ep2,ep3]}
     
     Return:
     ------
     batch_onsiteEs:
-        dict. {f: [tensor[es,ep], tensor[es,ep]]}
+        dict. 
+        e.g.: {f: [tensor[es,ep], tensor[es,ep]]} or {f: [tensor[es,ep1,ep2,ep3], tensor[es,ep1,ep2,ep3]]}.
     """
     batch_onsiteEs = {}
 
