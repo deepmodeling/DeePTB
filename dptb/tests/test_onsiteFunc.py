ba2134c71dc8be56615b6f126d13daf5754da406
qqgu <guqq@pku.edu.cn>
2022-09-02 02:01:45 +0800

    🧪 test(test_hamil_eig_sk, test_index_map, test_onsiteFunc, test_structure): add or modify tests for the onsiteE changement.
    1. add the test for index mapping.
    2. modify the test for structue with onsitemode
    3. test the onsiteFunc for onsite E split mode

--- dptb/tests/test_onsiteFunc.py
+++ dptb/tests/test_onsiteFunc.py
@@ -4,30 +4,58 @@ import numpy as np
 from dptb.nnsktb.onsiteFunc import loadOnsite, onsiteFunc
 
 
-def test_loadOnsite():
-    onsite_map = {'N': {'2s': [0], '2p': [1]}, 'B': {'2s': [0], '2p': [1]}}
-    onsitedb = loadOnsite(onsite_map)
-    assert list(onsitedb.keys()) == ['N', 'B']
-    assert onsitedb['N'].shape == th.Size([2])
-    assert onsitedb['B'].shape == th.Size([2])
-    assert th.abs(onsitedb['N'] -th.tensor([-0.6769242 , -0.26596692])).sum() < 1e-6
-    assert th.abs(onsitedb['B'] -th.tensor([-0.34482, -0.13648])).sum() < 1e-6
-
-    with pytest.raises(AssertionError):
-        onsite_map = {'N1': {'2s': [0], '2p': [1]}, 'B': {'2s': [0], '2p': [1]}}
-        loadOnsite(onsite_map)
-
-        onsite_map = {'N': {'2s': [0], '2p': [1], '3s': [0]}, 'B': {'2s': [0], '2p': [1]}}
-        loadOnsite(onsite_map)
-
-def test_onsiteFunc():
+class TestOnsiteUniform:
     batch_bonds_onsite= {0: np.array([[0, 7, 0, 7, 0, 0, 0, 0],
                                       [0, 5, 1, 5, 1, 0, 0, 0]])}
     onsite_map = {'N': {'2s': [0], '2p': [1]}, 'B': {'2s': [0], '2p': [1]}}
     
-    onsitedb = loadOnsite(onsite_map)
-    onsitesEdict = onsiteFunc(batch_bonds_onsite, onsitedb)
-    result = { 0:[th.tensor([-0.6769242 , -0.26596692]), th.tensor([-0.34482, -0.13648])] }
-    for ionsite in onsitesEdict:
-        for ie in range(len(onsitesEdict[ionsite])):
-            assert (onsitesEdict[ionsite][ie] == result[ionsite][ie]).all()
+    def test_loadOnsite(self):
+        onsitedb = loadOnsite(self.onsite_map)
+        assert list(onsitedb.keys()) == ['N', 'B']
+        assert onsitedb['N'].shape == th.Size([2])
+        assert onsitedb['B'].shape == th.Size([2])
+        assert th.abs(onsitedb['N'] -th.tensor([-0.6769242 , -0.26596692])).sum() < 1e-6
+        assert th.abs(onsitedb['B'] -th.tensor([-0.34482, -0.13648])).sum() < 1e-6
+
+        with pytest.raises(AssertionError):
+            onsite_map = {'N1': {'2s': [0], '2p': [1]}, 'B': {'2s': [0], '2p': [1]}}
+            loadOnsite(onsite_map)
+
+            onsite_map = {'N': {'2s': [0], '2p': [1], '3s': [0]}, 'B': {'2s': [0], '2p': [1]}}
+            loadOnsite(onsite_map)
+
+    def test_onsiteFunc(self):
+        onsitedb = loadOnsite(self.onsite_map)
+        onsitesEdict = onsiteFunc(self.batch_bonds_onsite, onsitedb)
+        result = { 0:[th.tensor([-0.6769242 , -0.26596692]), th.tensor([-0.34482, -0.13648])] }
+        for ionsite in onsitesEdict:
+            for ie in range(len(onsitesEdict[ionsite])):
+                assert (onsitesEdict[ionsite][ie] == result[ionsite][ie]).all()
+
+class TestOnsiteSplit:
+    batch_bonds_onsite= {0: np.array([[0, 7, 0, 7, 0, 0, 0, 0],
+                                      [0, 5, 1, 5, 1, 0, 0, 0]])}
+    onsite_map = {'N': {'2s': [0], '2p': [1,2,3]}, 'B': {'2s': [0], '2p': [1,2,3]}}
+
+    def test_loadOnsite(self):
+        onsitedb = loadOnsite(self.onsite_map)
+        assert list(onsitedb.keys()) == ['N', 'B']
+        assert onsitedb['N'].shape == th.Size([4])
+        assert onsitedb['B'].shape == th.Size([4])
+        assert th.abs(onsitedb['N'] -th.tensor([-0.6769242 , -0.26596692, -0.26596692, -0.26596692])).sum() < 1e-6
+        assert th.abs(onsitedb['B'] -th.tensor([-0.34482, -0.13648, -0.13648, -0.13648])).sum() < 1e-6
+
+        with pytest.raises(AssertionError):
+            onsite_map = {'N1': {'2s': [0], '2p': [1,2,3]}, 'B': {'2s': [0], '2p': [1,2,3]}}
+            loadOnsite(onsite_map)
+
+            onsite_map = {'N1': {'2s': [0], '2p': [1,2,3],'3s':[4]}, 'B': {'2s': [0], '2p': [1,2,3]}}
+            loadOnsite(onsite_map)
+
+    def test_onsiteFunc(self):
+        onsitedb = loadOnsite(self.onsite_map)
+        onsitesEdict = onsiteFunc(self.batch_bonds_onsite, onsitedb)
+        result = { 0:[th.tensor([-0.6769242 , -0.26596692, -0.26596692, -0.26596692]), th.tensor([-0.34482, -0.13648, -0.13648, -0.13648])] }
+        for ionsite in onsitesEdict:
+            for ie in range(len(onsitesEdict[ionsite])):
+                assert (onsitesEdict[ionsite][ie] == result[ionsite][ie]).all()
