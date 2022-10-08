178d2ac79ca7229ca5ff6bd8be0ea7d4082396cc
qqgu <guqq@pku.edu.cn>
2022-10-08 15:57:19 +0800

    fix test onsitemdoe uniform

--- dptb/tests/test_sknet.py
+++ dptb/tests/test_sknet.py
@@ -7,7 +7,9 @@ class TestSKnet:
     onsite_num = {'N':4,'B':3}
     bond_neurons = {'nhidden':5,'nout':4}
     onsite_neurons = {'nhidden':6}
-    model = SKNet(reducted_skint_types, onsite_num, bond_neurons, onsite_neurons)
+    onsitemode='uniform'
+    model = SKNet(skint_types=reducted_skint_types, onsite_num=onsite_num, bond_neurons=bond_neurons, 
+                    onsite_neurons=onsite_neurons,onsitemode=onsitemode)
 
     def test_bond(self):
 
@@ -23,7 +25,7 @@ class TestSKnet:
             assert ikey in self.reducted_skint_types
             assert coeff[ikey].shape == torch.Size([self.bond_neurons['nout']])
 
-    def test_onsite(self):
+    def test_onsite_uniform(self):
 
         paras = list(self.model.onsite_net.parameters())
         assert len(paras) == 4
