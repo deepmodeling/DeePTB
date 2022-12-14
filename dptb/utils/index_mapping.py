29ecf3e453b802b2411c7a25fec2117aa5ae7737
qqgu <guqq@pku.edu.cn>
2022-12-14 16:25:47 +0800

    docs(index mappings): add the docstrings for the index mappings functions.

--- dptb/utils/index_mapping.py
+++ dptb/utils/index_mapping.py
@@ -6,18 +6,43 @@ import numpy as np
 
 
 class Index_Mapings(object):
+    ''' creat index mappings for networks outs and the corresponding physical parameters.
+
+        Paras 
+        -----
+        proj_atom_anglr_m: 
+            the projection atoms angular momentum: eg: proj_atom_anglr_m = {'B':['s'],'N':['s','p']}
+    '''
+
     def __init__(self, proj_atom_anglr_m=None):
         self.AnglrMID = anglrMId
         if  proj_atom_anglr_m is not None:
             self.update(proj_atom_anglr_m = proj_atom_anglr_m)
 
     def update(self, proj_atom_anglr_m):
-        # bond and env type can get from stuct class.
+        # bondtype, means the atoms types for bond. here ['N', 'B']
         self.bondtype = get_uniq_symbol(list(proj_atom_anglr_m.keys()))
         # projected angular momentum. get from struct class.
         self.ProjAnglrM = proj_atom_anglr_m
 
     def Bond_Ind_Mapings(self):
+        ''' creat index mappings for networks outs and the hoppings.
+
+        Output: 
+        -------
+        bond_index_map: dict the index mapping for bond hoppings. 
+        bond_num_hops: dict the number of hops for each bond.
+        e.g.
+            for proj_atom_anglr_m = {'B':['s'],'N':['s','p']}, the output will be:
+        
+                bond_index_map = {'N-N': {'s-s': [0], 's-p': [1], 'p-s': [1], 'p-p': [2, 3]},
+                                  'N-B': {'s-s': [0], 'p-s': [1]},
+                                  'B-N': {'s-s': [0], 's-p': [1]},
+                                  'B-B': {'s-s': [0]}}
+
+                bond_num_hops = {'N-N': 4, 'N-B': 2, 'B-N': 2, 'B-B': 1}
+        '''
+        
         bond_index_map = {}
         bond_num_hops = {}
         for it in range(len(self.bondtype)):
@@ -58,6 +83,21 @@ class Index_Mapings(object):
         return bond_index_map, bond_num_hops
     
     def _Onsite_Ind_Mapings(self):
+        ''' creat index mappings for networks outs and the onsite energy.
+
+        Output: 
+        -------
+        onsite_index_map: dict the index mapping for onsite energy. 
+        onsite_num: dict the number of onsite energy for each atom type.
+        e.g.
+            for proj_atom_anglr_m = {'B':['s'],'N':['s','p']}, the output will be:
+        
+                onsite_index_map = {'N': {'s': [0], 'p': [1]}, 'B': {'s': [0]}}
+
+                onsite_num = {'N': 2, 'B': 1}
+        Note: here for this mode, the orbital s, p, d all assumed to de degenerated values for onsite energy. 
+               Therefore, for each orbial only 1 value is enough.
+        '''
         onsite_index_map = {}
         onsite_num = {}
         for it in range(len(self.bondtype)):
@@ -77,6 +117,21 @@ class Index_Mapings(object):
         return onsite_index_map, onsite_num
     
     def _Onsite_Ind_Mapings_OrbSplit(self):
+        ''' creat index mappings for networks outs and the onsite energy for split mode.
+
+        Output: 
+        -------
+        onsite_index_map: dict the index mapping for onsite energy. 
+        onsite_num: dict the number of onsite energy for each atom type.
+        e.g.
+            for proj_atom_anglr_m = {'B':['s'],'N':['s','p']}, the output will be:
+        
+                onsite_index_map = {'N': {'s': [0], 'p': [1, 2, 3]}, 'B': {'s': [0]}}
+
+                onsite_num = {'N': 4, 'B': 1}
+        Note 1: here for this mode, onsite energy of the orbital l = s, p, d, for different m, ie px py pz for p or dxy dyz .. for d can be different.
+        Note 2: this mode is only for testing, not used in real production.
+        '''
         onsite_index_map = {}
         onsite_num = {}
         for it in range(len(self.bondtype)):
@@ -97,6 +152,28 @@ class Index_Mapings(object):
 
 
     def _OnsiteStrain_Ind_Mapings(self, atomtypes):
+        ''' creat index mappings for networks outs and the onsite energy for strain mode.
+
+        INPUT:
+        ------
+        atomtypes: list of atomtypes. e.g. ['N','B']
+
+        Output: 
+        -------
+        onsite_intgrl_index_map: dict the index mapping for onsite_intgrl. 
+        onsite_intgrl_num: dict the number of onsite_intgrl for each atom type.
+        e.g.
+            for proj_atom_anglr_m = {'B':['s'],'N':['s','p']}, the output will be:
+        
+                onsite_intgrl_index_map = {'N-N': {'s-s': [0], 's-p': [1], 'p-s': [1], 'p-p': [2, 3]},
+                                           'N-B': {'s-s': [0], 's-p': [1], 'p-s': [1], 'p-p': [2, 3]},
+                                           'B-N': {'s-s': [0]},
+                                           'B-B': {'s-s': [0]}}
+
+                onsite_intgrl_num =  {'N-N': 4, 'N-B': 4, 'B-N': 1, 'B-B': 1})
+        Note: here for this mode, onsite energy is treated as a block matrix in the hamiltonian,
+                 which can be formed as the hoppings part with the sk-like integrals.
+        '''
 
         onsite_intgrl_index_map = {}
         onsite_intgrl_num = {}
