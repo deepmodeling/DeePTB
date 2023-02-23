a5cfca353a28bb767bbccd0d404f7476271d95f2
qqgu <guqq@pku.edu.cn>
2023-02-23 17:43:23 +0800

    all input ase atom for band.cal

--- dptb/postprocess/bandstructure/band.py
+++ dptb/postprocess/bandstructure/band.py
@@ -2,12 +2,19 @@ import numpy as np
 from dptb.utils.tools import j_must_have
 from dptb.utils.make_kpoints  import ase_kpath, interp_kpath
 from ase.io import read
+import ase
 import matplotlib.pyplot as plt
 
 class bandcalc (object):
     def __init__ (self, apiHrk, run_opt, jdata):
         self.apiH = apiHrk
-        self.structase = read(run_opt['structure'])
+        if isinstance(run_opt['structure'],str):
+            self.structase = read(run_opt['structure'])
+        elif isinstance(run_opt['structure'],ase.Atoms):
+            self.structase = run_opt['structure']
+        else:
+            raise ValueError('structure must be ase.Atoms or str')
+        
         self.jdata = jdata
         self.results_path = run_opt.get('results_path')
         self.apiH.update_struct(self.structase)
