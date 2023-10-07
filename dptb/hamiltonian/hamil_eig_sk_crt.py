ac75502be9908b79c1a595cfcfb3775a31e29955
qqgu <guqq_phy@qq.com>
2023-10-07 10:50:07 +0800

    update hamil_eig_sk_crt.py: add comments about the onsite overlap matrix

--- dptb/hamiltonian/hamil_eig_sk_crt.py
+++ dptb/hamiltonian/hamil_eig_sk_crt.py
@@ -149,7 +149,8 @@ class HamilEig(RotationSK):
 
             sub_hamil_block = th.zeros([self.__struct__.proj_atomtype_norbs[iatype], self.__struct__.proj_atomtype_norbs[jatype]], dtype=self.dtype, device=self.device)
             if not self.use_orthogonal_basis:
-                #sub_over_block = th.zeros([self.__struct__.proj_atomtype_norbs[iatype], self.__struct__.proj_atomtype_norbs[jatype]], dtype=self.dtype, device=self.device)
+                # For non - orthogonal basis, the overlap matrix is needed. 
+                # but for the onsite, the overlap matrix is identity.
                 sub_over_block = th.eye(self.__struct__.proj_atomtype_norbs[iatype], dtype=self.dtype, device=self.device)
 
             
@@ -161,6 +162,7 @@ class HamilEig(RotationSK):
 
                 indx = self.__struct__.onsite_index_map[iatype][ish] # change onsite index map from {N:{s:}} to {N:{ss:, sp:}}
                 sub_hamil_block[ist:ist+norbi, ist:ist+norbi] = th.eye(norbi, dtype=self.dtype, device=self.device) * self.onsiteEs[ib][indx]
+                # For non - orthogonal basis, the onsite overlap is identity, we don't need to calculate it.
                 #if not self.use_orthogonal_basis:
                 #    sub_over_block[ist:ist+norbi, ist:ist+norbi] = th.eye(norbi, dtype=self.dtype, device=self.device) * self.onsiteSs[ib][indx]
                 ist = ist + norbi
