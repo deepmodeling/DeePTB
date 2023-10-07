edab7f826321dde1536161ab8d5c072f693cb0ab
qqgu <guqq_phy@qq.com>
2023-10-07 17:15:37 +0800

    example: updata NRL-TB example.

--- examples/NRL-TB/silicon/run.sh
+++ examples/NRL-TB/silicon/run.sh
@@ -1,1 +1,9 @@
-dptb run band_plot.json -sk -i  nrl_ckpt.json -o band
+
+# load json model to plot band structure
+dptb run band_jsonckpt.json  -sk -i  ./ckpt/nrl_ckpt.json -o band
+
+# load json model to further train model:
+#dptb train input_nrl.json -sk -i ./ckpt/nrl_ckpt.json -o ckpt
+
+# load the trained model to plot band structure
+#dptb run band_pthckpt.json -sk -i ./ckpt/nrl_ckpt.pth -o bandpth
