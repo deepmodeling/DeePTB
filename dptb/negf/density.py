b01e33488fcc776ecd3316e3e015d8d0a5b468ca
Season Zou <jijiezou@stu.pku.edu.cn>
2023-11-01 17:30:45 +0800

    add docstring to density.py

--- dptb/negf/density.py
+++ dptb/negf/density.py
@@ -9,7 +9,23 @@ import matplotlib.pyplot as plt
 
 log = logging.getLogger(__name__)
 
+'''
+The density class is used to calculate the density of states of the device.
+1. Density is the basic class, which contains the integrate method and slice method.
+2. Ozaki  is used to calculate the density of states using the Ozaki method.
+(At this stage, it only supports the calculation of the density with Ozaki method)
+'''
+
 class Density(object):
+    '''
+    Density is the basic class
+
+    Method
+    ----------
+    slice
+        generate a 2D grid of real-space density with Gaussian broadening.
+
+    '''
     def __init__(self) -> None:
         pass
 
@@ -17,6 +33,36 @@ class Density(object):
         pass
 
     def slice(self, device, density, fix_dim="z", vslice=0.3, h=0.01, sigma=0.05, plot=False, optimize=False):
+        '''generate a 2D grid of real-space density along transmission direction with Gaussian broadening.
+        
+        The `slice` function takes in a device structure and density, and slices it along a specified
+        dimension to generate a 2D grid of data points, which can be plotted if desired.
+        
+        Parameters
+        ----------
+        device
+            Device object that represents the device 
+        density
+            electron density of the device
+        fix_dim
+            transmission direction, default is "z"
+        vslice
+            the value at which the slice is taken in the fixed dimension. 
+        h
+            the step size for the grid spacing. 
+        sigma
+            The parameter "sigma" represents the standard deviation of the Gaussian distribution used in the
+        calculation of the density. 
+        plot
+            a boolean value that determines whether or not to generate a plot of the sliced density. 
+        optimize
+            a boolean flag that determines whether to optimize the slicing dimensions based on the density data. 
+        
+        Returns
+        -------
+            three variables: X, Y, and data.
+        
+        '''
         
         lx, ly, lz = device.structure.cell.array.max(axis=0)
         sx, sy, sz = device.structure.cell.array.min(axis=0)
@@ -56,8 +102,36 @@ class Density(object):
             plt.plot()
         
         return X, Y, data
-    
+
 class Ozaki(Density):
+    '''calculates the equilibrium and non-equilibrium density with Ozaki method.
+        
+    The `Ozaki` class is a subclass of `Density` that calculates the equilibrium and non-equilibrium
+    density matrices and returns the onsite density. The Ozaki method details can be found in
+    T. Ozaki, Continued Fraction Representation of the Fermi-Dirac Function for Large-Scale Electronic Structure 
+    Calculations, Phys. Rev. B 75, 035123 (2007).
+
+
+    Property
+    ----------
+    poles
+        the poles of Ozaki method
+    residues
+        the residues of Ozaki method
+    R
+        radius parameter in Ozaki method
+    n_gauss
+        the number of points in the Gauss-Legendre quadrature method
+
+
+    Method
+    ----------
+    integrate
+        calculates the equilibrium and non-equilibrium density matrices for a given k-point.
+    on_site_density
+        calculate the onsite density for a given device and density matrix.
+    
+    '''
     def __init__(self, R, M_cut, n_gauss):
         super(Ozaki, self).__init__()
         self.poles, self.residues = ozaki_residues(M_cut)
@@ -66,6 +140,26 @@ class Ozaki(Density):
         self.n_gauss = n_gauss
 
     def integrate(self, device, kpoint, eta_lead=1e-5, eta_device=0.):
+        '''calculates the equilibrium and non-equilibrium density matrices for a given k-point.
+        
+        Parameters
+        ----------
+        device
+            the Device Object of the device for which the integration is being performed. 
+        kpoint
+            point in the Brillouin zone of the lead. It is used to calculate the self-energy and 
+            Green's function for the given kpoint.
+        eta_lead
+            the broadening parameter for the leads in the calculation of the self-energy.
+        eta_device
+            the broadening parameter for the device in the calculation of the Green function.
+        
+        Returns
+        -------
+            The function `integrate` returns two variables: `DM_eq` and `DM_neq`.
+            DM_eq is the equilibrium density matrix, and DM_neq is the non-equilibrium density matrix.
+        
+        '''
         kBT = device.kBT
         # add 0th order moment
         poles = 1j* self.poles * kBT + device.lead_L.mu - device.mu # left lead expression for rho_eq
@@ -102,6 +196,21 @@ class Ozaki(Density):
         return DM_eq, DM_neq
     
     def get_density_onsite(self, device, DM):
+        '''calculate the onsite density for a given device and density matrix.
+        
+        Parameters
+        ----------
+        device
+            the Device Object of the device for which the integration is being performed. 
+        DM
+           a given density matrix.
+        
+        Returns
+        -------
+            the onsite density, which is a tensor containing the positions of atoms in the device structure and
+        the corresponding density values for each atom.
+        
+        '''
         # assume DM is a cubic tensor
         if len(DM.shape) == 2:
             DM = DM.diag()
