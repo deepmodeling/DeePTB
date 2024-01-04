5eaad71aaf37ec19954e1efd56d22083e0ffb011
AsymmetryChou <181240085@smail.nju.edu.cn>
2024-01-04 17:31:28 +0800

    add find_atom_index

--- dptb/negf/poisson_grid_init.py
+++ dptb/negf/poisson_grid_init.py
@@ -8,6 +8,7 @@ class grid(object):
         self.yg = np.around(yg,decimals=5)
         self.zg = np.around(zg,decimals=5)
         # xa,ya,za are the coordinates of the atoms
+        self.Na = len(xa) # number of atoms
         uxa = np.unique(xa)
         uya = np.unique(ya)
         uza = np.unique(za)
@@ -17,3 +18,14 @@ class grid(object):
         self.z = np.unique(np.concatenate((uza,self.zg),0)).sort()
         self.Np = int(len(self.x)*len(self.y)*len(self.z))
         print('Number of grid points: ',self.Np)
+
+        self.atom_index = self.find_atom_index(xa,ya,za)
+
+    def find_atom_index(self,xa,ya,za):
+        # find the index of the atoms in the grid
+        swap = np.zeros(self.Na)
+        for i in range(self.Na):
+            for j in range(self.Np):
+                if xa[i]==self.x[j] and ya[i]==self.y[j] and za[i]==self.z[j]:
+                    swap[i] = j
+        return swap
