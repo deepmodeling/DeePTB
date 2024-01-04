f84c016031618de91b44a287f77ec84a0144d001
Sharp Londe <93334987+SharpLonde@users.noreply.github.com>
2024-01-04 22:05:09 +0800

    Unified dataset IO (#13)
    * Prototype code for loading Hamiltonian
    
    * add 'ABACUSDataset' in data module
    
    * modified "basis.dat" storage & can load overlap
    
    * recover some original dataset settings
    
    * add ABACUSDataset in init
    
    * Add the in memory version of ABACUSDataset
    
    * add ABACUSInMemoryDataset in data package
    
    * Added `DefaultDataset` and unified `ABACUSDataset`
    
    * improved DefaultDataset & add `dptb data` entrypoint for preprocess
    
    * update `build_dataset`
    
    * update `data` entrypoint
    
    * Unified dataset IO & added ASE trajectory support
    
    * Add support to save `.pth` files with different `info.json` settings.
    
    * Bug fix in dealing with "ase" info.
    
    * updated `argcheck` for setinfo.
    
    * added setinfo check when building dataset.
    
    * file IO improvements
    
    * bug fix in loading `info.json`

--- dptb/entrypoints/data.py
+++ dptb/entrypoints/data.py
@@ -14,33 +14,26 @@ def data(
 
     # ABACUS parsing input like:
     # {  "type": "ABACUS",
-    #    "root": "foo/bar",
     #    "parse_arguments": {
-    #        "input_dir": "alice/bob",
+    #        "input_path": "alice_*/*_bob/system_No_*",
     #        "preprocess_dir": "charlie/david",
     #        "only_overlap": false, 
     #        "get_Hamiltonian": true, 
     #        "add_overlap": true, 
     #        "get_eigenvalues": true } }
     if jdata["type"] == "ABACUS":
-        root = jdata["root"]
         abacus_args = jdata["parse_arguments"]
-        assert abacus_args.get("input_dir") is not None, "ABACUS calculation results MUST be provided."
-
-        if abacus_args.get("preprocess_dir") is None:
-            # create a new preprocess dir under root if not given
-            print("Creating new preprocess dictionary...")
-            os.mkdir(os.path.join(root, "preprocess"))
-            abacus_args["preprocess_dir"] = os.path.join(root, "preprocess")
+        assert abacus_args.get("input_path") is not None, "ABACUS calculation results MUST be provided."
+        assert abacus_args.get("preprocess_dir") is not None, "Please assign a dictionary to store preprocess files."
 
         print("Begin parsing ABACUS output...")
-        h5_filenames = recursive_parse(**abacus_args)
+        recursive_parse(**abacus_args)
         print("Finished parsing ABACUS output.")
         
-        # write all h5 files to be used in building AtomicData
-        with open(os.path.join(abacus_args["preprocess_dir"], "AtomicData_file.txt"), "w") as f:
-            for filename in h5_filenames:
-                f.write(filename + "\n")
+        ## write all h5 files to be used in building AtomicData
+        #with open(os.path.join(abacus_args["preprocess_dir"], "AtomicData_file.txt"), "w") as f:
+        #    for filename in h5_filenames:
+        #        f.write(filename + "\n")
 
     else:
         raise Exception("Not supported software output.")
