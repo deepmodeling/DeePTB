fix k-resolved transmission calculation bug and add kpoints sampling function especially for NEGF (#59)
* Hartree2eV unit constant keep the same as dptb-negf

* add transpose in lead_property.py

* add  if condition for kpoints in lead_property.py

* add kpoint-resolved cal in device_property.py

* add kmesh_sampling_negf in make_kpoints.py

* add gamma center and time symmetry to argcheck

* output all negf related info into one file

* add output_freq for uni_grid

* add kpoints info output

* add docstring to time_symmetry_reduce

* simplify time_symmetry_reduce

* fix I-V profile as a k-averaged value

* fix docstring in lead_property

* add muti-kpoints T calculation (graphene) in test_negf_run.py

* add test_negf_kmesh_sampling.py

* fix test_tbtrans_init.py bug due to updating of ev2hartree

* remove unnecssary print

* add docstring to sigmaLR2Gamma

* check the consistency of kmesh and pbc in NEGF.py

* update time_symmetry_reduce in make_kpoints.py

* add comments in MP sampling

* update docstring and comments in time_reversal

dptb/negf/device_property.py
@@ -87,6 +87,8 @@ class DeviceProperty(object):
         self.e_T = e_T
         self.efermi = efermi
         self.mu = self.efermi
+        self.kpoint = None
+        self.V = None
     
     def set_leadLR(self, lead_L, lead_R):
         '''initialize the left and right lead in Device object
@@ -132,7 +134,7 @@ class DeviceProperty(object):
             energy = torch.tensor(energy, dtype=torch.complex128)
 
         self.block_tridiagonal = block_tridiagonal
-        self.kpoint = kpoint
+        # self.kpoint = kpoint
 
         # if V is not None:
         #     HD_ = self.attachPotential(HD, SD, V)
@@ -146,8 +148,16 @@ class DeviceProperty(object):
         else:
             self.V = 0.
         
-        if not hasattr(self, "hd") or not hasattr(self, "sd"):
+        if not hasattr(self, "hd") or not hasattr(self, "sd") or self.kpoint is None:
             self.hd, self.sd, _, _, _, _ = self.hamiltonian.get_hs_device(kpoint, self.V, block_tridiagonal)
+            self.kpoint = torch.tensor(kpoint)
+        elif not torch.allclose(self.kpoint, torch.tensor(kpoint), atol=1e-5):
+            self.hd, self.sd, _, _, _, _ = self.hamiltonian.get_hs_device(kpoint, self.V, block_tridiagonal)
+            self.kpoint = torch.tensor(kpoint)
+
+
+
+
         s_in = [torch.zeros(i.shape).cdouble() for i in self.hd]
         
         # for i, e in tqdm(enumerate(ee), desc="Compute green functions: "):

dptb/negf/lead_property.py
@@ -73,6 +73,7 @@ class LeadProperty(object):
         self.e_T = e_T
         self.efermi = efermi
         self.mu = self.efermi - self.voltage
+        self.kpoint = None
 
     def self_energy(self, kpoint, energy, eta_lead: float=1e-5, method: str="Lopez-Sancho"):
         '''calculate and loads the self energy and surface green function at the given kpoint and energy.
@@ -84,18 +85,26 @@ class LeadProperty(object):
         energy
             specific energy value.
         eta_lead : 
-            the broadening parameter for calculating lead self-energy.
+            the broadening parameter for calculating lead surface green function.
         method : 
             specify the method for calculating the self energy. At this stage it only supports "Lopez-Sancho".
         
         '''
         assert len(np.array(kpoint).reshape(-1)) == 3
+        
         # according to given kpoint and e_mesh, calculating or loading the self energy and surface green function to self.
         if not isinstance(energy, torch.Tensor):
             energy = torch.tensor(energy)
 
-        if not hasattr(self, "HL"):
+                
+
+        if not hasattr(self, "HL") or self.kpoint is None:
             self.HL, self.HLL, self.HDL, self.SL, self.SLL, self.SDL = self.hamiltonian.get_hs_lead(kpoint, tab=self.tab, v=self.voltage)
+            self.kpoint = torch.tensor(kpoint)
+        elif not torch.allclose(self.kpoint, torch.tensor(kpoint), atol=1e-5):
+            self.HL, self.HLL, self.HDL, self.SL, self.SLL, self.SDL = self.hamiltonian.get_hs_lead(kpoint, tab=self.tab, v=self.voltage)
+            self.kpoint = torch.tensor(kpoint)
+            
 
         self.se, _ = selfEnergy(
             ee=energy,
@@ -118,15 +127,15 @@ class LeadProperty(object):
         Parameters
         ----------
         se
-            The parameter "se" represents a complex number.
+            The parameter "se" represents self energy, a complex matrix.
         
         Returns
         -------
         Gamma
-            The Gamma function.
+            The Gamma function, $\Gamma = -1j(se-se^\dagger)$
         
         '''
-        return -1j * (se - se.conj())
+        return -1j * (se - se.conj().T)
     
     def fermi_dirac(self, x) -> torch.Tensor:
         return 1 / (1 + torch.exp((x - self.mu)/ self.kBT))

dptb/postprocess/NEGF.py
@@ -17,7 +17,7 @@ from dptb.utils.constants import Boltzmann, eV2J
 import os
 from dptb.utils.tools import j_must_have
 import numpy as np
-from dptb.utils.make_kpoints import kmesh_sampling
+from dptb.utils.make_kpoints import kmesh_sampling_negf
 import logging
 
 log = logging.getLogger(__name__)
@@ -47,10 +47,24 @@ class NEGF(object):
         self.e_fermi = jdata["e_fermi"]
         self.stru_options = j_must_have(jdata, "stru_options")
         self.pbc = self.stru_options["pbc"]
+
+        # check the consistency of the kmesh and pbc
+        assert len(self.pbc) == 3, "pbc should be a list of length 3"
+        for i in range(3):
+            if self.pbc[i] == False and self.jdata["stru_options"]["kmesh"][i] > 1:
+                raise ValueError("kmesh should be 1 for non-periodic direction")
+            elif self.pbc[i] == False and self.jdata["stru_options"]["kmesh"][i] == 0:
+                self.jdata["stru_options"]["kmesh"][i] = 1
+                log.info(msg="Warning! kmesh should be set to 1 for non-periodic direction")
+            elif self.pbc[i] == True and self.jdata["stru_options"]["kmesh"][i] == 0:
+                raise ValueError("kmesh should be > 0 for periodic direction")
+
         if not any(self.pbc):
-            self.kpoints = np.array([[0,0,0]])
+            self.kpoints,self.wk = np.array([[0,0,0]]),np.array([1.])
         else:
-            self.kpoints = kmesh_sampling(self.jdata["stru_options"]["kmesh"])
+            self.kpoints,self.wk = kmesh_sampling_negf(self.jdata["stru_options"]["kmesh"], 
+                                                       self.jdata["stru_options"]["gamma_center"],
+                                                       self.jdata["stru_options"]["time_reversal_symmetry"],)
 
         self.unit = jdata["unit"]
         self.scf = jdata["scf"]
@@ -154,16 +168,27 @@ class NEGF(object):
         else:
             pass
         
-        # computing output properties
-        for ik, k in enumerate(self.kpoints):
-            self.out = {}
-            log.info(msg="Properties computation at k = [{:.4f},{:.4f},{:.4f}]".format(float(k[0]),float(k[1]),float(k[2])))
+        self.out['k']=[]; self.out['wk']=[]
+        if hasattr(self, "uni_grid"): self.out["E"] = self.uni_grid
 
+        #  output kpoints information
+        log.info(msg="------ k-point for NEGF -----\n")
+        log.info(msg="Gamma Center: {0}".format(self.jdata["stru_options"]["gamma_center"])+"\n")
+        log.info(msg="Time Reversal: {0}".format(self.jdata["stru_options"]["time_reversal_symmetry"])+"\n")
+        log.info(msg="k-points Num: {0}".format(len(self.kpoints))+"\n")
+        log.info(msg="k-points weights: {0}".format(self.wk)+"\n")
+        log.info(msg="--------------------------------\n")
+
+        for ik, k in enumerate(self.kpoints):
+            self.out["k"].append(k)
+            self.out['wk'].append(self.wk[ik])
+            log.info(msg="Properties computation at k = [{:.4f},{:.4f},{:.4f}]".format(float(k[0]),float(k[1]),float(k[2])))
             # computing properties that is functions of E
             if hasattr(self, "uni_grid"):
-                self.out["k"] = k
-                for e in self.uni_grid:
-                    log.info(msg="computing green's function at e = {:.3f}".format(float(e)))
+                output_freq = int(len(self.uni_grid)/10)
+                for ie,e in enumerate(self.uni_grid):
+                    if ie % output_freq == 0:
+                        log.info(msg="computing green's function at e = {:.3f}".format(float(e)))
                     leads = self.stru_options.keys()
                     for ll in leads:
                         if ll.startswith("lead"):
@@ -182,29 +207,42 @@ class NEGF(object):
                         )
 
                     if self.out_dos:
-                        prop = self.out.setdefault("DOS", [])
-                        prop.append(self.compute_DOS(k))
+                        # prop = self.out['DOS'].setdefault(str(k), [])
+                        # prop.append(self.compute_DOS(k))
+                        prop = self.out.setdefault('DOS', {})
+                        propk = prop.setdefault(str(k), [])
+                        propk.append(self.compute_DOS(k))
                     if self.out_tc or self.out_current_nscf:
-                        prop = self.out.setdefault("TC", [])
-                        prop.append(self.compute_TC(k))
+                        # prop = self.out['TC'].setdefault(str(k), [])
+                        # prop.append(self.compute_TC(k))
+                        prop = self.out.setdefault('T_k', {})
+                        propk = prop.setdefault(str(k), [])
+                        propk.append(self.compute_TC(k))
                     if self.out_ldos:
-                        prop = self.out.setdefault("LDOS", [])
-                        prop.append(self.compute_LDOS(k))
+                        # prop = self.out['LDOS'].setdefault(str(k), [])
+                        # prop.append(self.compute_LDOS(k))
+                        prop = self.out.setdefault('LDOS', {})
+                        propk = prop.setdefault(str(k), [])
+                        propk.append(self.compute_LDOS(k))
 
             if self.out_dos:
-                self.out["DOS"] = torch.stack(self.out["DOS"])
+                self.out["DOS"][str(k)] = torch.stack(self.out["DOS"][str(k)])
+
             if self.out_tc or self.out_current_nscf:
-                self.out["TC"] = torch.stack(self.out["TC"])
+                self.out["T_k"][str(k)] = torch.stack(self.out["T_k"][str(k)])
             
-            if self.out_current_nscf:
-                self.out["BIAS_POTENTIAL_NSCF"], self.out["CURRENT_NSCF"] = self.compute_current_nscf(k, self.uni_grid, self.out["TC"])
+            # if self.out_current_nscf:
+                # self.out["BIAS_POTENTIAL_NSCF"][str(k)], self.out["CURRENT_NSCF"][str(k)] \
+                #     = self.compute_current_nscf(k, self.uni_grid, self.out["TC"][str(k)])
             
             # computing properties that are not functions of E (improvement can be made here in properties related to integration of energy window of fermi functions)
             if self.out_current:
                 pass
 
             if self.out_density or self.out_potential:
-                self.out["DM_eq"], self.out["DM_neq"] = self.compute_density(k)
+                prop_DM_eq = self.out.setdefault('DM_eq', {})
+                prop_DM_neq = self.out.setdefault('DM_neq', {})
+                prop_DM_eq[str(k)], prop_DM_neq[str(k)] = self.compute_density(k)
             
             if self.out_potential:
                 pass
@@ -231,11 +269,18 @@ class NEGF(object):
                         )
                     
                     lcurrent += self.int_weight[i] * self.compute_lcurrent(k)
-                self.out["LOCAL_CURRENT"] = lcurrent
+                
+                prop_local_current = self.out.setdefault('LOCAL_CURRENT', {})
+                prop_local_current[str(k)] = lcurrent
 
-            torch.save(self.out, self.results_path+"/negf.k{}.out.pth".format(ik))
+        self.out["k"] = np.array(self.out["k"])
+        self.out['T_avg'] = torch.tensor(self.out['wk']) @ torch.stack(list(self.out["T_k"].values()))
 
-            # plotting
+        if self.out_current_nscf:
+            self.out["BIAS_POTENTIAL_NSCF"], self.out["CURRENT_NSCF"] \
+                = self.compute_current_nscf(k, self.uni_grid, self.out["T_avg"])
+        torch.save(self.out, self.results_path+"/negf.out.pth")
+
             
     
     def fermi_dirac(self, x) -> torch.Tensor: