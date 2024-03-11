30769e310587285b74de72568a52c38bc9a8d1c6
Yinzhanghao Zhou <64253517+floatingCatty@users.noreply.github.com>
2024-03-11 17:10:43 +0800

    fix to align the checkpoint output index with the log output index (#77)
    * fix to align the checkpoint output index with the log output index
    
    * fix test
    
    * fix test
    
    * fix test

--- dptb/tests/test_sktb.py
+++ dptb/tests/test_sktb.py
@@ -17,7 +17,7 @@ def test_nnsk_valence():
 def test_nnsk_strain_polar():
     INPUT_file = "./dptb/tests/data/test_sktb/input/input_strain_polar.json"
     output = "./dptb/tests/data/test_sktb/output"
-    init_model = "./dptb/tests/data/test_sktb/output/test_valence/checkpoint/nnsk.iter6.pth"
+    init_model = "./dptb/tests/data/test_sktb/output/test_valence/checkpoint/nnsk.iter5.pth"
 
     train(INPUT=INPUT_file, init_model=init_model, restart=None, train_soc=False,\
           output=output+"/test_strain_polar", log_level=5, log_path=output+"/test_strain_polar.log")
@@ -26,15 +26,15 @@ def test_nnsk_push():
     INPUT_file_rs = "./dptb/tests/data/test_sktb/input/input_push_rs.json"
     INPUT_file_w = "./dptb/tests/data/test_sktb/input/input_push_w.json"
     output = "./dptb/tests/data/test_sktb/output"
-    init_model = "./dptb/tests/data/test_sktb/output/test_strain_polar/checkpoint/nnsk.iter6.pth"
+    init_model = "./dptb/tests/data/test_sktb/output/test_strain_polar/checkpoint/nnsk.iter5.pth"
 
     train(INPUT=INPUT_file_rs, init_model=init_model, restart=None, train_soc=False,\
           output=output+"/test_push_rs", log_level=5, log_path=output+"/test_push_rs.log")
     train(INPUT=INPUT_file_w, init_model=init_model, restart=None, train_soc=False,\
           output=output+"/test_push_w", log_level=5, log_path=output+"/test_push_w.log")
     
-    model_rs = torch.load("./dptb/tests/data/test_sktb/output/test_push_rs/checkpoint/nnsk.iter11.pth")
-    model_w = torch.load("./dptb/tests/data/test_sktb/output/test_push_w/checkpoint/nnsk.iter11.pth")
+    model_rs = torch.load("./dptb/tests/data/test_sktb/output/test_push_rs/checkpoint/nnsk.iter10.pth")
+    model_w = torch.load("./dptb/tests/data/test_sktb/output/test_push_w/checkpoint/nnsk.iter10.pth")
     # test push limits
     # 10 epoch, 0.01 step, 1 period -> 0.05 added.
     assert np.isclose(model_rs["config"]["model_options"]["nnsk"]["hopping"]["rs"], 2.65)
@@ -43,7 +43,7 @@ def test_nnsk_push():
 def test_md():
     INPUT_file = "./dptb/tests/data/test_sktb/input/input_md.json"
     output = "./dptb/tests/data/test_sktb/output"
-    init_model = "./dptb/tests/data/test_sktb/output/test_push_w/checkpoint/nnsk.iter11.pth"
+    init_model = "./dptb/tests/data/test_sktb/output/test_push_w/checkpoint/nnsk.iter10.pth"
 
     train(INPUT=INPUT_file, init_model=init_model, restart=None, train_soc=False,\
           output=output+"/test_md", log_level=5, log_path=output+"/test_md.log")
