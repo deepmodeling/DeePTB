1853a31a2d2c8047865ba4dc9288500bd8d87cbe
qqgu <guqq_phy@qq.com>
2024-03-16 17:35:04 +0800

    update 2 files and create 1 file:
    1. update dptb.nn.build.py, deeptb.py
    fix the condition for choose the correct mode nnsk, mix and dptb.
    fix the input para in mix function
    
    2. create test_build_model.py file
    add test for build model from scratch,
    still missing:
     test for dptb-e3tb from scratch
     and test for all the cases from checkpoint, restart and init_model

---
+++ dptb/tests/test_build_model.py
@@ -0,0 +1,156 @@
+import pytest
+import torch
+from dptb.nn.nnsk import NNSK
+from dptb.nn.deeptb import DPTB, MIX
+from dptb.utils.tools import j_must_have
+from dptb.nn.build import build_model
+import os
+from pathlib import Path
+from dptb.data.transforms import OrbitalMapper
+
+
+rootdir = os.path.join(Path(os.path.abspath(__file__)).parent, "data/out")
+
+
+def test_build_nnsk_from_scratch():
+    run_options = {
+        "init_model": None,
+        "restart": None,
+        "train_soc": False,
+        "log_path": f"{rootdir}/log.txt",
+        "log_level": "INFO"
+    }
+    model_options = {
+        "nnsk": {
+            "onsite": {
+                "method": "strain",
+                "rs": 2.5,
+                "w": 0.3
+            },
+            "hopping": {
+                "method": "powerlaw",
+                "rs": 2.6,
+                "w": 0.35
+            },
+            "freeze": False,
+            "std": 0.01,
+            "push": None,
+        }
+    }
+    common_options = {
+        "basis": {
+            "Si": ["3s","3p"]
+        },
+        "device": "cpu",
+        "dtype": "float32",
+        "overlap": False,
+        "seed": 3982377700
+    }
+    statistics = None
+    model = build_model(run_options, model_options, common_options, statistics)
+
+    assert isinstance(model, NNSK)
+    assert model.device == "cpu"
+    assert model.dtype ==  getattr(torch, common_options["dtype"])
+    assert model.name == "nnsk"
+    assert model.transform == True
+    # OrbitalMapper(basis=common_options["basis"], method="sktb", device=common_options["device"])
+    assert hasattr(model, "idp_sk")
+    assert isinstance(model.idp_sk, OrbitalMapper)
+    assert model.idp_sk.basis == common_options["basis"]
+    assert model.idp_sk.method == "sktb"
+    
+def test_build_model_MIX_from_checkpoint():
+    run_options = {
+        "init_model": None,
+        "restart": None,
+        "train_soc": False,
+        "log_path": f"{rootdir}/log.txt",
+        "log_level": "INFO"
+    }
+    model_options = {
+        "nnsk": {
+            "onsite": {
+                "method": "strain",
+                "rs": 2.5,
+                "w": 0.3
+            },
+            "hopping": {
+                "method": "powerlaw",
+                "rs": 2.6,
+                "w": 0.35
+            },
+            "freeze": False,
+            "std": 0.01,
+            "push": None,
+        },
+        "embedding": {
+            "method": "se2",
+            "rs": 2.5,
+            "rc": 5.0,
+            "radial_net": {
+                "neurons": [10,20,30],
+                "activation": "tanh",
+                "if_batch_normalized": False
+            },
+            "n_axis": None
+        },
+        "prediction": {
+            "method": "sktb",
+            "neurons": [16,16,16],
+            "activation": "tanh",
+            "if_batch_normalized": False
+        }
+    }
+    common_options = {
+        "basis": {
+            "Si": ["3s","3p"]
+        },
+        "device": "cpu",
+        "dtype": "float32",
+        "overlap": False,
+        "seed": 3982377700
+    }
+    statistics = None
+
+    model = build_model(run_options, model_options, common_options, statistics)
+
+    assert isinstance(model, MIX)
+    assert model.name == "mix"
+    assert model.dptb.method == 'sktb'
+    assert model.dptb.name == 'dptb'
+    assert model.nnsk.name == 'nnsk'
+
+    assert model.nnsk.transform == False
+    assert model.dptb.transform == False
+
+def test_build_model_failure():
+    run_options = {
+        "init_model": None,
+        "restart": None,
+        "train_soc": False,
+        "log_path": f"{rootdir}/log.txt",
+        "log_level": "INFO"
+    }
+    model_options = {}
+    common_options = {}
+
+    with pytest.raises(ValueError) as excinfo:
+        build_model(run_options, model_options, common_options)
+    assert "You need to provide model_options and common_options" in str(excinfo.value)
+    
+    model_options = {"embedding":True, "nnsk":True}
+    common_options = {}
+    with pytest.raises(ValueError) as excinfo:
+        build_model(run_options, model_options, common_options)
+    assert "You need to provide model_options and common_options" in str(excinfo.value)
+    
+    model_options = {"embedding":True, "nnsk":True}
+    common_options = {"basis": {"Si": ["3s", "3p"]}}
+    with pytest.raises(ValueError) as excinfo:
+        build_model(run_options, model_options, common_options)
+    assert "Model_options are not set correctly!" in str(excinfo.value)
+
+
+#TODO: add test for dptb-e3tb from scratch
+#TODO: add test for all the cases from checkpoint, restart and init_model
