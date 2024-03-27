c0ca6ac0ef7b11f677dab61f0423e94ee2655a58
zhanghao <zhouyinzhanghao@gmail.com>
2024-03-27 10:10:39 +0800

    modify tbplas test

--- dptb/tests/test_tbplas.py
+++ dptb/tests/test_tbplas.py
@@ -45,10 +45,10 @@ def test_tbplas_get_cell(root_directory):
     cell = tbplas.get_cell(data=dataset, e_fermi=-7.724611085233356)
 
     # check cell onsite value
-    assert cell.get_orbital(0).energy == -5.0404769961265075
-    assert cell.get_orbital(1).energy == 3.4503467496010316
-    assert cell.get_orbital(-1).energy == 11.223119538602496
+    assert abs(cell.get_orbital(0).energy + 5.0404769961265075) < 1e-5
+    assert abs(cell.get_orbital(1).energy - 3.4503467496010316) < 1e-5
+    assert abs(cell.get_orbital(-1).energy - 11.223119538602496) < 1e-5
 
     # check cell hopping value
-    assert cell.get_hopping(orb_i=0, orb_j=9, rn=(0,0,0)) == 1.8239758014678955
-    assert cell.get_hopping(orb_i=0, orb_j=10, rn=(0,0,0)) == 1.259192943572998
+    assert abs(cell.get_hopping(orb_i=0, orb_j=9, rn=(0,0,0)) - 1.8239758014678955) < 1e-5
+    assert abs(cell.get_hopping(orb_i=0, orb_j=10, rn=(0,0,0)) - 1.259192943572998) < 1e-5
