0056579ddabaf3f88f6895cfca37eefbf2d8738c
qqgu <guqq_phy@qq.com>
2024-04-09 00:56:04 +0800

    update nrl silicon exampe. to test dptb run command

--- examples/NRL-TB/silicon/run.sh
+++ examples/NRL-TB/silicon/run.sh
@@ -1,9 +1,11 @@
 
 # load json model to plot band structure
-dptb run band_jsonckpt.json  -sk -i  ./ckpt/nrl_ckpt.json -o band
+dptb run in_band.json -i  ./outv2spd/nrl_ckpt.json -o band
+
+
+# load  trained pth model to plot band structure
+# dptb run in_band.json -i  ./nrlspd_frzoverlap/checkpoint/nnsk.best.pth -o band
 
 # load json model to further train model:
-#dptb train input_nrl.json -sk -i ./ckpt/nrl_ckpt.json -o ckpt
+# dptb train input_spd_fz.json -i  ./outv2spd/nrl_ckpt.json -o ckpt
 
-# load the trained model to plot band structure
-#dptb run band_pthckpt.json -sk -i ./ckpt/nrl_ckpt.pth -o bandpth
