5fd133a0d2f79887f8cdb9a9579c9af3282f4eaf
AsymmetryChou <181240085@smail.nju.edu.cn>
2024-04-11 21:56:35 +0800

    modify test_negf_device_property

--- dptb/tests/test_negf_device_property.py
+++ dptb/tests/test_negf_device_property.py
@@ -85,115 +85,91 @@ def test_negf_Device(root_directory):
     assert deviceprop.lead_R.tab=="lead_R"
 
 
-    # calculate Self energy and Green function
-    stru_options = j_must_have(negf_json['task_options'], "stru_options")
+   # calculate Self energy and Green function
+    task_options = negf_json['task_options']
+    device = deviceprop
+
+    stru_options = j_must_have(task_options, "stru_options")
     leads = stru_options.keys()
     for ll in leads:
         if ll.startswith("lead"): #calculate surface green function at E=0
-            getattr(deviceprop, ll).self_energy(
+            getattr(device, ll).self_energy(
                 energy=torch.tensor([0]), 
                 kpoint=kpoints[0], 
-                eta_lead=negf_json['task_options']["eta_lead"],
-                method=negf_json['task_options']["sgf_solver"]
+                eta_lead=task_options["eta_lead"],
+                method=task_options["sgf_solver"]
                 )
 
         # check left and right leads' self-energy
-    lead_L_se_standard=torch.tensor([[0.0000e+00-0.61690134j,  0.0000e+00+0.0000j,  0.0000e+00+0.0000j,0.0000e+00+0.0000j],
+    lead_L_se_standard=torch.tensor([[-3.3171e-07-0.6096j,  0.0000e+00+0.0000j,  0.0000e+00+0.0000j,0.0000e+00+0.0000j],
         [ 0.0000e+00+0.0000j,  0.0000e+00+0.0000j,  0.0000e+00+0.0000j,0.0000e+00+0.0000j],
         [ 0.0000e+00+0.0000j,  0.0000e+00+0.0000j,  0.0000e+00+0.0000j,0.0000e+00+0.0000j],
         [ 0.0000e+00+0.0000j,  0.0000e+00+0.0000j,  0.0000e+00+0.0000j,0.0000e+00+0.0000j]], dtype=torch.complex128)
     lead_R_se_standard=torch.tensor([[ 0.0000e+00+0.0000j,  0.0000e+00+0.0000j,  0.0000e+00+0.0000j,0.0000e+00+0.0000j],
         [ 0.0000e+00+0.0000j,  0.0000e+00+0.0000j,  0.0000e+00+0.0000j,0.0000e+00+0.0000j],
         [ 0.0000e+00+0.0000j,  0.0000e+00+0.0000j,  0.0000e+00+0.0000j,0.0000e+00+0.0000j],
-        [ 0.0000e+00+0.0000j,  0.0000e+00+0.0000j,  0.0000e+00+0.0000j,0.0000e+00-0.51628502j]], dtype=torch.complex128)
+        [ 0.0000e+00+0.0000j,  0.0000e+00+0.0000j,  0.0000e+00+0.0000j,-3.3171e-07-0.6096j]], dtype=torch.complex128)
 
-    
-    assert  abs(deviceprop.lead_L.se-lead_L_se_standard).max()<1e-5
-    assert  abs(deviceprop.lead_R.se-lead_R_se_standard).max()<1e-5
+    assert  abs(device.lead_L.se-lead_L_se_standard).max()<1e-5
+    assert  abs(device.lead_R.se-lead_R_se_standard).max()<1e-5
 
-    deviceprop.cal_green_function(  energy=torch.tensor([0]),   #calculate device green function at E=0
+    device.cal_green_function(  energy=torch.tensor([0]),   #calculate device green function at E=0
                             kpoint=kpoints[0], 
-                            eta_device=negf_json['task_options']["eta_device"], 
-                            block_tridiagonal=negf_json['task_options']["block_tridiagonal"]
+                            eta_device=task_options["eta_device"], 
+                            block_tridiagonal=task_options["block_tridiagonal"]
                             )
 
     #check  green functions' results
-    assert list(deviceprop.greenfuncs.keys())==['g_trans', 'grd', 'grl', 'gru', 'gr_left', 'gnd', 'gnl',\
+    assert list(device.greenfuncs.keys())==['g_trans', 'grd', 'grl', 'gru', 'gr_left', 'gnd', 'gnl',\
                                         'gnu', 'gin_left', 'gpd', 'gpl', 'gpu', 'gip_left']
-    g_trans= torch.tensor([[ 0.00000000-0.74812411j, -0.88334131-0.00000000j,
-          0.00000000+0.74812499j,  0.88333889+0.00000000j],
-        [-0.88334131-0.00000000j,  0.00000000-0.89392734j,
-         -0.75709057-0.00000000j, -0.00000000+0.89392489j],
-        [-0.00000000+0.74812499j, -0.75709057-0.00000000j,
-          0.00000000-0.74812586j, -0.88333993-0.00000000j],
-        [ 0.88333889+0.00000000j,  0.00000000+0.89392489j,
-         -0.88333993+0.00000000j, -0.00000000-0.89392244j]],dtype=torch.complex128)
-    
-    grd= [torch.tensor([[ 0.00000000-0.74812411j, -0.88334131-0.00000000j,
-          0.00000000+0.74812499j,  0.88333889+0.00000000j],
-        [-0.88334131-0.00000000j,  0.00000000-0.89392734j,
-         -0.75709057-0.00000000j, -0.00000000+0.89392489j],
-        [-0.00000000+0.74812499j, -0.75709057-0.00000000j,
-          0.00000000-0.74812586j, -0.88333993-0.00000000j],
-        [ 0.88333889+0.00000000j,  0.00000000+0.89392489j,
-         -0.88333993+0.00000000j, -0.00000000-0.89392244j]],dtype=torch.complex128)]
+    g_trans= torch.tensor([[ 1.0983e-11-8.2022e-01j, -8.2022e-01+4.4634e-07j,8.9264e-07+8.2022e-01j,  8.2022e-01-1.3390e-06j],
+            [-8.2022e-01+4.4634e-07j, -3.6607e-12-8.2022e-01j,-8.2021e-01+4.4631e-07j,  8.9264e-07+8.2022e-01j],
+            [ 8.9264e-07+8.2022e-01j, -8.2021e-01+4.4631e-07j,-3.6607e-12-8.2022e-01j, -8.2022e-01+4.4634e-07j],
+            [ 8.2022e-01-1.3390e-06j,  8.9264e-07+8.2022e-01j,-8.2022e-01+4.4634e-07j,  1.0983e-11-8.2022e-01j]],dtype=torch.complex128)
+    grd= [torch.tensor([[ 1.0983e-11-8.2022e-01j, -8.2022e-01+4.4634e-07j,8.9264e-07+8.2022e-01j,  8.2022e-01-1.3390e-06j],
+            [-8.2022e-01+4.4634e-07j, -3.6607e-12-8.2022e-01j,-8.2021e-01+4.4631e-07j,  8.9264e-07+8.2022e-01j],
+            [ 8.9264e-07+8.2022e-01j, -8.2021e-01+4.4631e-07j,-3.6607e-12-8.2022e-01j, -8.2022e-01+4.4634e-07j],
+            [ 8.2022e-01-1.3390e-06j,  8.9264e-07+8.2022e-01j,-8.2022e-01+4.4634e-07j,  1.0983e-11-8.2022e-01j]],dtype=torch.complex128)]
 
-    assert  abs(g_trans-deviceprop.greenfuncs['g_trans']).max()<1e-5
-    assert  abs(grd[0]-deviceprop.greenfuncs['grd'][0]).max()<1e-5
-    assert deviceprop.greenfuncs['grl'] == []
-    assert deviceprop.greenfuncs['gru'] == []
+    assert  abs(g_trans-device.greenfuncs['g_trans']).max()<1e-5
+    assert  abs(grd[0]-device.greenfuncs['grd'][0]).max()<1e-5
+    assert device.greenfuncs['grl'] == []
+    assert device.greenfuncs['gru'] == []
 
-    gr_left= [torch.tensor([[ 0.00000000-0.74812411j, -0.88334131-0.00000000j,
-          0.00000000+0.74812499j,  0.88333889+0.00000000j],
-        [-0.88334131-0.00000000j,  0.00000000-0.89392734j,
-         -0.75709057-0.00000000j, -0.00000000+0.89392489j],
-        [-0.00000000+0.74812499j, -0.75709057-0.00000000j,
-          0.00000000-0.74812586j, -0.88333993-0.00000000j],
-        [ 0.88333889+0.00000000j,  0.00000000+0.89392489j,
-         -0.88333993+0.00000000j, -0.00000000-0.89392244j]],
-            dtype=torch.complex128)]
+    gr_left= [torch.tensor([[ 1.0983e-11-8.2022e-01j, -8.2022e-01+4.4634e-07j,8.9264e-07+8.2022e-01j,  8.2022e-01-1.3390e-06j],
+            [-8.2022e-01+4.4634e-07j, -3.6607e-12-8.2022e-01j,-8.2021e-01+4.4631e-07j,  8.9264e-07+8.2022e-01j],
+            [ 8.9264e-07+8.2022e-01j, -8.2021e-01+4.4631e-07j,-3.6607e-12-8.2022e-01j, -8.2022e-01+4.4634e-07j],
+            [ 8.2022e-01-1.3390e-06j,  8.9264e-07+8.2022e-01j,-8.2022e-01+4.4634e-07j,  1.0983e-11-8.2022e-01j]],dtype=torch.complex128)]
 
-    gnd = [torch.tensor([[ 0.74812411+0.00000000e+00j,  0.00000000-5.55111512e-17j,
-         -0.74812499+0.00000000e+00j,  0.00000000+5.55111512e-17j],
-        [ 0.00000000+1.11022302e-16j,  0.89392734+0.00000000e+00j,
-          0.00000000+5.55111512e-17j, -0.89392489+0.00000000e+00j],
-        [-0.74812499+0.00000000e+00j,  0.00000000-1.11022302e-16j,
-          0.74812586+0.00000000e+00j,  0.00000000+1.11022302e-16j],
-        [ 0.00000000-5.55111512e-17j, -0.89392489+0.00000000e+00j,
-          0.00000000-1.11022302e-16j,  0.89392244+0.00000000e+00j]],dtype=torch.complex128)]
+    gnd = [torch.tensor([[ 8.2022e-01+0.0000e+00j, -4.4634e-07+2.2204e-16j,-8.2022e-01-3.1764e-22j,  1.3390e-06-5.5511e-17j],
+        [-4.4634e-07-2.7756e-16j,  8.2022e-01+2.6470e-23j, -4.4631e-07+2.7756e-16j, -8.2022e-01-2.3823e-22j],
+        [-8.2022e-01+2.9117e-22j, -4.4631e-07-2.2204e-16j, 8.2022e-01+7.9409e-23j, -4.4634e-07+1.1102e-16j],
+        [ 1.3390e-06+5.5511e-17j, -8.2022e-01+2.1176e-22j, -4.4634e-07-1.1102e-16j,  8.2022e-01+0.0000e+00j]],dtype=torch.complex128)]
 
-    assert  abs(gr_left[0]-deviceprop.greenfuncs['gr_left'][0]).max()<1e-5
-    assert  abs(gnd[0]-deviceprop.greenfuncs['gnd'][0]).max()<1e-5
-    assert deviceprop.greenfuncs['gnl'] == []
-    assert deviceprop.greenfuncs['gnu'] == []
+    assert  abs(gr_left[0]-device.greenfuncs['gr_left'][0]).max()<1e-5
+    assert  abs(gnd[0]-device.greenfuncs['gnd'][0]).max()<1e-5
+    assert device.greenfuncs['gnl'] == []
+    assert device.greenfuncs['gnu'] == []
 
-    gin_left=[torch.tensor([[ 0.74812411+0.00000000e+00j,  0.00000000-5.55111512e-17j,
-         -0.74812499+0.00000000e+00j,  0.00000000+5.55111512e-17j],
-        [ 0.00000000+1.11022302e-16j,  0.89392734+0.00000000e+00j,
-          0.00000000+5.55111512e-17j, -0.89392489+0.00000000e+00j],
-        [-0.74812499+0.00000000e+00j,  0.00000000-1.11022302e-16j,
-          0.74812586+0.00000000e+00j,  0.00000000+1.11022302e-16j],
-        [ 0.00000000-5.55111512e-17j, -0.89392489+0.00000000e+00j,
-          0.00000000-1.11022302e-16j,  0.89392244+0.00000000e+00j]],dtype=torch.complex128)]
+    gin_left=[torch.tensor([[ 8.2022e-01+0.0000e+00j, -4.4634e-07+2.2204e-16j, -8.2022e-01-3.1764e-22j,  1.3390e-06-5.5511e-17j],
+        [-4.4634e-07-2.7756e-16j,  8.2022e-01+2.6470e-23j, -4.4631e-07+2.7756e-16j, -8.2022e-01-2.3823e-22j],
+        [-8.2022e-01+2.9117e-22j, -4.4631e-07-2.2204e-16j, 8.2022e-01+7.9409e-23j, -4.4634e-07+1.1102e-16j],
+        [ 1.3390e-06+5.5511e-17j, -8.2022e-01+2.1176e-22j,-4.4634e-07-1.1102e-16j,  8.2022e-01+0.0000e+00j]],dtype=torch.complex128)]
+    assert  abs(gin_left[0]-device.greenfuncs['gin_left'][0]).max()<1e-5
 
+    assert device.greenfuncs['gpd']== None
+    assert device.greenfuncs['gpl']== None
+    assert device.greenfuncs['gpu']== None
+    assert device.greenfuncs['gip_left']== None
 
-    assert  abs(gin_left[0]-deviceprop.greenfuncs['gin_left'][0]).max()<1e-5
+    Tc=device._cal_tc_() #transmission
+    assert abs(Tc-1)<1e-5
 
-    assert deviceprop.greenfuncs['gpd']== None
-    assert deviceprop.greenfuncs['gpl']== None
-    assert deviceprop.greenfuncs['gpu']== None
-    assert deviceprop.greenfuncs['gip_left']== None
-
-    Tc=deviceprop._cal_tc_() #transmission
-    assert abs(Tc-1)<1e-2
-
-    dos = deviceprop._cal_dos_()
-    dos_standard = torch.tensor(2.090723, dtype=torch.float64)
+    dos = device._cal_dos_()
+    dos_standard = torch.tensor(2.0887, dtype=torch.float64)
     assert abs(dos-dos_standard)<1e-4
 
-    ldos = deviceprop._cal_ldos_()
-    torch.set_printoptions(precision=6)
-    print(ldos)
+    ldos = device._cal_ldos_()
     ldos_standard = torch.tensor([0.2611, 0.2611, 0.2611, 0.2611], dtype=torch.float64)*2
     assert abs(ldos_standard-ldos).max()<1e-4
 
