5067295a14205d87695aade475e06be1f2d1bec6
AsymmetryChou <181240085@smail.nju.edu.cn>
2024-04-16 09:33:45 +0800

    add unit test for tbtrans

--- dptb/tests/test_tbtrans_init.py
+++ dptb/tests/test_tbtrans_init.py
@@ -4,7 +4,7 @@ from dptb.negf.device_property import DeviceProperty
 from dptb.nn.build import build_model
 from dptb.negf.negf_hamiltonian_init import NEGFHamiltonianInit
 import torch
-# from dptb.postprocess.tbtrans_init import TBTransInputSet
+from dptb.postprocess.tbtrans_init import TBTransInputSet
 from dptb.utils.tools import j_loader
 import numpy as np
 
@@ -22,23 +22,15 @@ def test_tbtrans_init(root_directory):
         pytest.skip('sisl is not installed which is necessary for TBtrans Input Generation. Therefore, skipping test_tbtrans_init.')
 
     model_ckpt = f'{root_directory}/dptb/tests/data/test_tbtrans/best_nnsk_b3.600_c3.600_w0.300.json'
-    results_path=f'{root_directory}/dptb/tests/data/test_tbtrans/'
+    results_path=f'{root_directory}/dptb/tests/data/test_tbtrans/test_output'
     input_path = root_directory +"/dptb/tests/data/test_tbtrans/negf_tbtrans.json"
-    structure=root_directory +"/dptb/tests/data/test_tbtrans/structure_tbtrans.vasp"
+    structure=root_directory +"/dptb/tests/data/test_tbtrans/test_hBN_zigzag_struct.xyz"
 
    
     negf_json = json.load(open(input_path))
+    model_json = json.load(open(model_ckpt))
     model = build_model(model_ckpt,model_options=negf_json['model_options'],common_options=negf_json['common_options'])
 
-
-    hamiltonian = NEGFHamiltonianInit(model=model,
-                                    AtomicData_options=negf_json['AtomicData_options'], 
-                                    structure=structure,
-                                    pbc_negf = negf_json['task_options']["stru_options"]['pbc'], 
-                                    stru_options = negf_json['task_options']['stru_options'],
-                                    unit = negf_json['task_options']['unit'], 
-                                    results_path=results_path,
-                                    torch_device = torch.device('cpu'))
 
       
     # apihost = NNSKHost(checkpoint=model_ckpt, config=config)
@@ -46,21 +38,26 @@ def test_tbtrans_init(root_directory):
     # apihost.build()
     # apiHrk = NN2HRK(apihost=apihost, mode='nnsk')
 
-    run_opt = {
-            "run_sk": True,
-            "init_model":model_ckpt,
-            "results_path":f'{root_directory}/dptb/tests/data/test_tbtrans/',
-            "structure":f'{root_directory}/dptb/tests/data/test_tbtrans/test_hBN_zigzag_struct.xyz',
-            "log_path": '/data/DeepTB/dptb_Zjj/DeePTB/dptb/tests/data/test_tbtrans/output',
-            "log_level": 5,
-            "use_correction":False
-        }
+    # run_opt = {
+    #         "run_sk": True,
+    #         "init_model":model_ckpt,
+    #         "results_path":f'{root_directory}/dptb/tests/data/test_tbtrans/',
+    #         "structure":f'{root_directory}/dptb/tests/data/test_tbtrans/test_hBN_zigzag_struct.xyz',
+    #         "log_path": '/data/DeepTB/dptb_Zjj/DeePTB/dptb/tests/data/test_tbtrans/output',
+    #         "log_level": 5,
+    #         "use_correction":False
+    #     }
 
-    jdata = j_loader(config)
-    jdata = jdata['task_options']
-    tbtrans_hBN = TBTransInputSet(apiHrk=apiHrk,run_opt=run_opt, jdata=jdata)
+    # jdata = j_loader(config)
+    # jdata = jdata['task_options']
+    tbtrans_hBN = TBTransInputSet(model = model,
+                                 AtomicData_options = negf_json['AtomicData_options'], 
+                                 structure = structure,
+                                 results_path= results_path,
+                                 basis_dict=negf_json['common_options']['basis'],
+                                 **negf_json['task_options'])
 
-    # check _orbitals_name_get
+    # # check _orbitals_name_get
     element_orbital_name = tbtrans_hBN._orbitals_name_get(['2s', '2p']) 
     assert element_orbital_name == ['2s', '2py', '2pz', '2px']
     element_orbital_name = tbtrans_hBN._orbitals_name_get(['3s', '3p', 'd*']) 
@@ -71,21 +68,21 @@ def test_tbtrans_init(root_directory):
     tbtrans_hBN.hamil_get_write(write_nc=False)
     assert (tbtrans_hBN.allbonds_all[0].detach().numpy()-np.array([5, 0, 5, 0, 0, 0, 0])).max()<1e-5
     assert (tbtrans_hBN.allbonds_all[50].detach().numpy()-np.array([ 5,  2,  5, 18,  0,  0, -1])).max()<1e-5
-    assert (tbtrans_hBN.hamil_block_all[0].detach().numpy()-   np.array([[-0.73303634,  0.        ,  0.        ,  0.        ],
+    assert (tbtrans_hBN.hamil_block_all[0].detach().numpy()*1.0000/13.605662285137/2-   np.array([[-0.73303634,  0.        ,  0.        ,  0.        ],
                                                                         [ 0.        ,  0.04233637,  0.        ,  0.        ],
                                                                         [ 0.        ,  0.        ,  0.04233636,  0.        ],
                                                                         [ 0.        ,  0.        ,  0.        , -0.27156556]])).max()<1e-5
-    assert (tbtrans_hBN.hamil_block_all[50].detach().numpy()-np.array([[-0.03164609, -0.        ,  0.02028139, -0.        ],
+    assert (tbtrans_hBN.hamil_block_all[50].detach().numpy()*1.0000/13.605662285137/2-np.array([[-0.03164609, -0.        ,  0.02028139, -0.        ],
                                                                         [ 0.        ,  0.00330366,  0.        ,  0.        ],
                                                                         [-0.02028139,  0.        , -0.05393751,  0.        ],
                                                                         [ 0.        ,  0.        ,  0.        ,  0.00330366]])).max()<1e-5
     assert (tbtrans_hBN.allbonds_lead_L[0].detach().numpy()-np.array([5, 0, 5, 0, 0, 0, 0])).max()<1e-5
     assert (tbtrans_hBN.allbonds_lead_L[50].detach().numpy()-np.array([5, 4, 7, 7, 0, 0, 0])).max()<1e-5
-    assert (tbtrans_hBN.hamil_block_lead_L[0].detach().numpy()-np.array([[-0.73303634,  0.        ,  0.        ,  0.        ],
+    assert (tbtrans_hBN.hamil_block_lead_L[0].detach().numpy()*1.0000/13.605662285137/2-np.array([[-0.73303634,  0.        ,  0.        ,  0.        ],
                                                                         [ 0.        ,  0.04233637,  0.        ,  0.        ],
                                                                         [ 0.        ,  0.        ,  0.04233636,  0.        ],
                                                                         [ 0.        ,  0.        ,  0.        , -0.27156556]])).max()<1e-5
-    assert (tbtrans_hBN.hamil_block_lead_L[50].detach().numpy()-np.array([[ 0.1145315 , -0.06116847,  0.10594689,  0.        ],
+    assert (tbtrans_hBN.hamil_block_lead_L[50].detach().numpy()*1.0000/13.605662285137/2-np.array([[ 0.1145315 , -0.06116847,  0.10594689,  0.        ],
                                                                             [ 0.15539739, -0.04634972,  0.22751862,  0.        ],
                                                                             [-0.26915616,  0.22751862, -0.30906558,  0.        ],
                                                                             [-0.        ,  0.        ,  0.        ,  0.08500822]])).max()<1e-5
@@ -100,25 +97,27 @@ def test_tbtrans_init(root_directory):
     G_eigs = G_eigs.apply.array.eigh() -Ef
     M_eigs = M_eigs.apply.array.eigh() -Ef
    
-    G_eigs_right = np.array([[-19.95431228, -17.10836511, -17.10836511, -16.91249093, -11.20658215,
- -10.01096331, -10.01096331,  -8.11484357,  -8.11484357,  -7.60482165,
-  -6.65543971,  -3.79243033,  -3.79243032,  -3.22893608,  -3.22893608,
-  -3.17565711,   3.18687914,   3.2401581,    3.2401581,    6.4529848,
-   7.70578579,   7.91102607 , 10.91061078 , 10.91061078 , 23.6481713,
-  23.6481713,   28.30797724 , 28.30797738 , 28.65762375 , 30.78500847,
-  33.2545355 ,  33.2545355 ]])
+    G_eigs_correct = np.array([[-19.9606056213, -17.1669273376, -17.0796546936, -16.8952560425,
+        -11.2022151947, -10.1263637543,  -9.9411945343,  -8.1748561859,
+         -8.0049209595,  -7.6048202515,  -6.6505813599,  -3.7902746201,
+         -3.7882986069,  -3.2289390564,  -3.2289323807,  -3.1756639481,
+          3.1868720055,   3.2401518822,   3.2401537895,   6.4502696991,
+          7.7058019638,   7.8654155731,  10.8850431442,  10.9806480408,
+         23.6024589539,  23.6807479858,  28.3076782227,  28.3091220856,
+         28.6845626831,  30.7864990234,  33.2417755127,  33.2541275024]])
 
-    M_eigs_right = np.array([[-18.69719147, -18.69719147, -16.91249065, -16.91249065, -11.20658214,
- -11.20658214,  -7.4476675,   -7.4476675 ,  -6.65543987  ,-6.65543987,
-  -5.79288269,  -5.79288269,  -5.21972335 , -5.21972335 , -3.17565711,
-  -3.17565711,   3.18687914,   3.18687914  , 5.84897577 ,  5.84897577,
-   7.74721734,   7.74721734,   7.91102615 ,  7.91102615 , 28.12953979,
-  28.12953979,  28.65762339,  28.65762339,  29.54228118,  29.54228118,
-  30.78500872,  30.78500872]])
+    M_eigs_correct = np.array([[-18.7672195435, -18.6261577606, -16.9293708801, -16.9104366302,
+        -11.2307147980, -11.1986064911,  -7.5301399231,  -7.3464851379,
+         -6.6541309357,  -6.6479454041,  -5.7928838730,  -5.7928771973,
+         -5.2177238464,  -5.2155799866,  -3.1756563187,  -3.1756496429,
+          3.1868739128,   3.1868796349,   5.8489637375,   5.8489723206,
+          7.6596388817,   7.7445230484,   7.9219026566,   7.9709992409,
+         28.1335067749,  28.1563091278,  28.6396503448,  28.6454830170,
+         29.5444660187,  29.5520248413,  30.7856063843,  30.7873668671]])
 
     
-    assert (G_eigs[0]-G_eigs_right[0]).max()<1e-5
-    assert (M_eigs[0]-M_eigs_right[0]).max()<1e-5
+    assert (G_eigs[0]-G_eigs_correct[0]).max()<1e-4
+    assert (M_eigs[0]-M_eigs_correct[0]).max()<1e-4
 
 
 
