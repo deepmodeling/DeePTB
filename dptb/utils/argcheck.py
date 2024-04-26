8664b19386c796904c98f149eb4b38fea1ab938a
AsymmetryChou <181240085@smail.nju.edu.cn>
2024-04-26 11:08:10 +0800

    add poisson options in argcheck

--- dptb/utils/argcheck.py
+++ dptb/utils/argcheck.py
@@ -1021,18 +1021,76 @@ def fmm():
         Argument("err", [int, float], optional=True, default=1e-5, doc=doc_err)
     ]
 
-def  pyamg():
+def pyamg():
     doc_err = ""
-
+    doc_tolerance=""
+    doc_grid=""
+    doc_gate=""
+    doc_dielectric=""
+    doc_max_iter=""
+    doc_mix_rate=""
     return [
-        Argument("err", [int, float], optional=True, default=1e-5, doc=doc_err)
+        Argument("err", [int, float], optional=True, default=1e-5, doc=doc_err),
+        Argument("tolerance", [int, float], optional=True, default=1e-7, doc=doc_tolerance),
+        Argument("max_iter", int, optional=True, default=100, doc=doc_max_iter),
+        Argument("mix_rate", int, optional=True, default=0.25, doc=doc_mix_rate),
+        Argument("grid", dict, optional=False, sub_fields=grid(), doc=doc_grid),
+        Argument("gate_top", dict, optional=False, sub_fields=gate(), doc=doc_gate),
+        Argument("gate_bottom", dict, optional=False, sub_fields=gate(), doc=doc_gate),
+        Argument("dielectric_region", dict, optional=False, sub_fields=dielectric(), doc=doc_dielectric)
     ]
 
 def scipy():
     doc_err = ""
-
+    doc_tolerance=""
+    doc_grid=""
+    doc_gate=""
+    doc_dielectric=""
+    doc_max_iter=""
+    doc_mix_rate=""
     return [
-        Argument("err", [int, float], optional=True, default=1e-5, doc=doc_err)
+        Argument("err", [int, float], optional=True, default=1e-5, doc=doc_err),
+        Argument("tolerance", [int, float], optional=True, default=1e-7, doc=doc_tolerance),
+        Argument("max_iter", int, optional=True, default=100, doc=doc_max_iter),
+        Argument("mix_rate", int, optional=True, default=0.25, doc=doc_mix_rate),
+        Argument("grid", dict, optional=False, sub_fields=grid(), doc=doc_grid),
+        Argument("gate_top", dict, optional=False, sub_fields=gate(), doc=doc_gate),
+        Argument("gate_bottom", dict, optional=False, sub_fields=gate(), doc=doc_gate),
+        Argument("dielectric_region", dict, optional=False, sub_fields=dielectric(), doc=doc_dielectric)
+    ]
+
+def grid():
+    doc_xrange=""
+    doc_yrange=""
+    doc_zrange=""
+    return [
+        Argument("x_range", str, optional=False, doc=doc_xrange),
+        Argument("y_range", str, optional=False, doc=doc_yrange),
+        Argument("z_range", str, optional=False, doc=doc_zrange),
+    ]
+
+def gate():
+    doc_xrange=""
+    doc_yrange=""
+    doc_zrange=""
+    doc_voltage=""
+    return [
+        Argument("x_range", str, optional=False, doc=doc_xrange),
+        Argument("y_range", str, optional=False, doc=doc_yrange),
+        Argument("z_range", str, optional=False, doc=doc_zrange),
+        Argument("voltage", [int, float], optional=False, doc=doc_voltage)
+    ]
+
+def dielectric():
+    doc_xrange=""
+    doc_yrange=""
+    doc_zrange=""
+    doc_permittivity=""
+    return [
+        Argument("x_range", str, optional=False, doc=doc_xrange),
+        Argument("y_range", str, optional=False, doc=doc_yrange),
+        Argument("z_range", str, optional=False, doc=doc_zrange),
+        Argument("relative permittivity", [int, float], optional=False, doc=doc_permittivity)
     ]
 
 def run_options():
