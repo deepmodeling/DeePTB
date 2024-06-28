27fb2a899aa8c78f5b87265ce12d38e11e66dabf
zhanghao <zhouyinzhanghao@gmail.com>
2024-06-28 16:44:37 +0800

    move E3 statistics initialization into dataset, optmize nested tensor support in hr2hk and eigvals compute

--- dptb/nn/build.py
+++ dptb/nn/build.py
@@ -11,8 +11,7 @@ log = logging.getLogger(__name__)
 def build_model(
         checkpoint: str=None,
         model_options: dict={}, 
-        common_options: dict={}, 
-        statistics: dict=None
+        common_options: dict={}
         ):
     """
     The build model method should composed of the following steps:
@@ -141,23 +140,8 @@ def build_model(
     if from_scratch:
         if init_nnenv:
             model = NNENV(**model_options, **common_options)
-
-            # do initialization from statistics if NNENV is e3tb and statistics is provided
-            if model.method == "e3tb" and statistics is not None:
-                scalar_mask = torch.BoolTensor([ir.dim==1 for ir in model.idp.orbpair_irreps])
-                node_shifts = statistics["node"]["scalar_ave"]
-                node_scales = statistics["node"]["norm_ave"]
-                node_scales[:,scalar_mask] = statistics["node"]["scalar_std"]
-
-                edge_shifts = statistics["edge"]["scalar_ave"]
-                edge_scales = statistics["edge"]["norm_ave"]
-                edge_scales[:,scalar_mask] = statistics["edge"]["scalar_std"]
-                model.node_prediction_h.set_scale_shift(scales=node_scales, shifts=node_shifts)
-                model.edge_prediction_h.set_scale_shift(scales=edge_scales, shifts=edge_shifts)
-
         elif init_nnsk:
             model = NNSK(**model_options["nnsk"], **common_options)
-
         elif init_mixed:
             model = MIX(**model_options, **common_options)
         elif init_dftbsk:
