39435511fb57eaf25b4b0402da85b598a21f59d0
QG-phy <guqq_phy@qq.com>
2024-08-05 16:29:59 +0800

    refactor(build dataset): change build_dataset from function to a class instance and add from_model class function.
    note, compared to the previous build_dataset, this one is more flexible.
    previous build_dataset is a function. now i define a class DataBuilder and re-defined __call__ function.  then build_dataset is an instance of DataBuilder class. so i can use build_dataset.from_model() to build dataset from model. at the same time the previous way to use  build_dataset is still available. like build_dataset(...).

--- dptb/data/build.py
+++ dptb/data/build.py
@@ -14,7 +14,9 @@ from dptb.data import AtomicDataset, register_fields
 from dptb.utils import instantiate, get_w_prefix
 from dptb.utils.tools import j_loader
 from dptb.utils.argcheck import normalize_setinfo, normalize_lmdbsetinfo
+from dptb.utils.argcheck import collect_cutoffs 
 import logging
+
 
 log = logging.getLogger(__name__)
 
@@ -107,8 +109,11 @@ def dataset_from_config(config, prefix: str = "dataset") -> AtomicDataset:
 
     return instance
 
+class DatasetBuilder:
+    def __init__(self):
+        pass
 
-def build_dataset(
+    def __call__(self, 
         # set_options
         root: str,
         # dataset_options
@@ -128,153 +133,187 @@ def build_dataset(
         **kwargs,
         ):
     
-    """
-    Build a dataset based on the provided set options and common options.
+        """
+        Build a dataset based on the provided set options and common options.
 
-    Args:
-        - type (str): The type of dataset to build. Default is "DefaultDataset".
-        - root (str): The main directory storing all trajectory folders.
-        - prefix (str, optional): Load selected trajectory folders with the specified prefix.
-        - get_Hamiltonian (bool, optional): Load the Hamiltonian file to edges of the graph or not.
-        - get_eigenvalues (bool, optional): Load the eigenvalues to the graph or not.
-        e.g.     
-        type = "DefaultDataset",
-        root = "foo/bar/data_files_here",
-        prefix = "set"
+        Args:
+            - type (str): The type of dataset to build. Default is "DefaultDataset".
+            - root (str): The main directory storing all trajectory folders.
+            - prefix (str, optional): Load selected trajectory folders with the specified prefix.
+            - get_Hamiltonian (bool, optional): Load the Hamiltonian file to edges of the graph or not.
+            - get_eigenvalues (bool, optional): Load the eigenvalues to the graph or not.
+            e.g.     
+            type = "DefaultDataset",
+            root = "foo/bar/data_files_here",
+            prefix = "set"
 
-        - basis (str, optional): The basis for the OrbitalMapper.
+            - basis (str, optional): The basis for the OrbitalMapper.
 
-    Returns:
-        dataset: The built dataset.
+        Returns:
+            dataset: The built dataset.
 
-    Raises:
-        ValueError: If the dataset type is not supported.
-        Exception: If the info.json file is not properly provided for a trajectory folder.
-    """
-    dataset_type = type
-    # See if we can get a OrbitalMapper.
-    if basis is not None:
-        idp = OrbitalMapper(basis=basis)
-    else:
-        idp = None
-
-    if dataset_type in ["DefaultDataset", "DeePHDataset", "HDF5Dataset", "LMDBDataset"]:
-
-        # Explore the dataset's folder structure.
-        #include_folders = []
-        #for dir_name in os.listdir(root):
-        #    dir_path = os.path.join(root, dir_name)
-        #    if os.path.isdir(dir_path):
-        #        # If the `processed_dataset` or other folder is here too, they do not have the proper traj data files.
-        #        # And we will have problem in generating TrajData! 
-        #        # So we test it here: the data folder must have `.dat` or `.traj` file.
-        #        # If not, we will skip thi
-        #        if glob.glob(os.path.join(dir_path, '*.dat')) or glob.glob(os.path.join(dir_path, '*.traj')):
-        #            if prefix is not None:
-        #                if dir_name[:len(prefix)] == prefix:
-        #                    include_folders.append(dir_name)
-        #            else:
-        #                include_folders.append(dir_name)
-
-        assert prefix is not None, "The prefix is not provided. Please provide the prefix to select the trajectory folders."
-        prefix_folders = glob.glob(f"{root}/{prefix}{separator}*")
-        include_folders=[]
-        for idir in prefix_folders:
-            if os.path.isdir(idir):
-                if not glob.glob(os.path.join(idir, '*.dat')) \
-                    and not glob.glob(os.path.join(idir, '*.traj')) \
-                        and not glob.glob(os.path.join(idir, '*.h5')) \
-                            and not glob.glob(os.path.join(idir, '*.mdb')):
-                    raise Exception(f"{idir} does not have the proper traj data files. Please check the data files.")
-                include_folders.append(idir.split('/')[-1])
-        
-        assert isinstance(include_folders, list) and len(include_folders) > 0, "No trajectory folders are found. Please check the prefix."                
-            
-        # We need to check the `info.json` very carefully here.
-        # Different `info` points to different dataset, 
-        # even if the data files in `root` are basically the same.
-        info_files = {}
-
-        # See if a public info is provided.
-        #if "info.json" in os.listdir(root):
-        if os.path.exists(f"{root}/info.json"):
-            public_info = j_loader(os.path.join(root, "info.json"))
-            if dataset_type == "LMDBDataset":
-                public_info = {}
-                log.info("A public `info.json` file is provided, but will not be used  anymore for LMDBDataset.")
-            else:
-                public_info = normalize_setinfo(public_info)
-                log.info("A public `info.json` file is provided, and will be used by the subfolders who do not have their own `info.json` file.")
+        Raises:
+            ValueError: If the dataset type is not supported.
+            Exception: If the info.json file is not properly provided for a trajectory folder.
+        """
+        dataset_type = type
+        # See if we can get a OrbitalMapper.
+        if basis is not None:
+            idp = OrbitalMapper(basis=basis)
         else:
-            public_info = None
+            idp = None
 
-        # Load info in each trajectory folders seperately.
-        for file in include_folders:
-            #if "info.json" in os.listdir(os.path.join(root, file)):
-            
-            if dataset_type == "LMDBDataset":
-                info_files[file] = {}
-            elif os.path.exists(f"{root}/{file}/info.json"):
-                # use info provided in this trajectory.
-                info = j_loader(f"{root}/{file}/info.json")
-                info = normalize_setinfo(info)
-                info_files[file] = info
-            elif public_info is not None:  # not lmbd and no info in subfolder, then must use public info.
-                # use public info instead
-                # yaml will not dump correctly if this is not a deepcopy.
-                info_files[file] = deepcopy(public_info)
-            else:  # not lmdb no info in subfolder and no public info. then raise error.
-                log.error(f"for {dataset_type} type, the info.json is not properly provided for `{file}`")
-                raise ValueError(f"for {dataset_type} type, the info.json is not properly provided for `{file}`")
-            
-        # We will sort the info_files here.
-        # The order itself is not important, but must be consistant for the same list.
-        info_files = {key: info_files[key] for key in sorted(info_files)}
+        if dataset_type in ["DefaultDataset", "DeePHDataset", "HDF5Dataset", "LMDBDataset"]:
+            assert prefix is not None, "The prefix is not provided. Please provide the prefix to select the trajectory folders."
+            prefix_folders = glob.glob(f"{root}/{prefix}{separator}*")
+            include_folders=[]
+            for idir in prefix_folders:
+                if os.path.isdir(idir):
+                    if not glob.glob(os.path.join(idir, '*.dat')) \
+                        and not glob.glob(os.path.join(idir, '*.traj')) \
+                            and not glob.glob(os.path.join(idir, '*.h5')) \
+                                and not glob.glob(os.path.join(idir, '*.mdb')):
+                        raise Exception(f"{idir} does not have the proper traj data files. Please check the data files.")
+                    include_folders.append(idir.split('/')[-1])
+
+            assert isinstance(include_folders, list) and len(include_folders) > 0, "No trajectory folders are found. Please check the prefix."                
+
+            # We need to check the `info.json` very carefully here.
+            # Different `info` points to different dataset, 
+            # even if the data files in `root` are basically the same.
+            info_files = {}
+
+            # See if a public info is provided.
+            #if "info.json" in os.listdir(root):
+            if os.path.exists(f"{root}/info.json"):
+                public_info = j_loader(os.path.join(root, "info.json"))
+                if dataset_type == "LMDBDataset":
+                    public_info = {}
+                    log.info("A public `info.json` file is provided, but will not be used  anymore for LMDBDataset.")
+                else:
+                    public_info = normalize_setinfo(public_info)
+                    log.info("A public `info.json` file is provided, and will be used by the subfolders who do not have their own `info.json` file.")
+            else:
+                public_info = None
+
+            # Load info in each trajectory folders seperately.
+            for file in include_folders:
+                #if "info.json" in os.listdir(os.path.join(root, file)):
+
+                if dataset_type == "LMDBDataset":
+                    info_files[file] = {}
+                elif os.path.exists(f"{root}/{file}/info.json"):
+                    # use info provided in this trajectory.
+                    info = j_loader(f"{root}/{file}/info.json")
+                    info = normalize_setinfo(info)
+                    info_files[file] = info
+                elif public_info is not None:  # not lmbd and no info in subfolder, then must use public info.
+                    # use public info instead
+                    # yaml will not dump correctly if this is not a deepcopy.
+                    info_files[file] = deepcopy(public_info)
+                else:  # not lmdb no info in subfolder and no public info. then raise error.
+                    log.error(f"for {dataset_type} type, the info.json is not properly provided for `{file}`")
+                    raise ValueError(f"for {dataset_type} type, the info.json is not properly provided for `{file}`")
+
+            # We will sort the info_files here.
+            # The order itself is not important, but must be consistant for the same list.
+            info_files = {key: info_files[key] for key in sorted(info_files)}
+        
+            for ikey in info_files:
+                info_files[ikey].update({'r_max': r_max, 'er_max': er_max, 'oer_max': oer_max})
+
+            if dataset_type == "DeePHDataset":
+                dataset = DeePHE3Dataset(
+                    root=root,
+                    type_mapper=idp,
+                    get_Hamiltonian=get_Hamiltonian,
+                    get_eigenvalues=get_eigenvalues,
+                    info_files = info_files
+                )
+            elif dataset_type == "DefaultDataset":
+                dataset = DefaultDataset(
+                    root=root,
+                    type_mapper=idp,
+                    get_Hamiltonian=get_Hamiltonian,
+                    get_overlap=get_overlap,
+                    get_DM=get_DM,
+                    get_eigenvalues=get_eigenvalues,
+                    info_files = info_files
+                )
+            elif dataset_type == "HDF5Dataset":
+                dataset = HDF5Dataset(
+                    root=root,
+                    type_mapper=idp,
+                    get_Hamiltonian=get_Hamiltonian,
+                    get_overlap=get_overlap,
+                    get_DM=get_DM,
+                    get_eigenvalues=get_eigenvalues,
+                    info_files = info_files
+                )
+            elif dataset_type == "LMDBDataset":
+                dataset = LMDBDataset(
+                root=root,
+                type_mapper=idp,
+                orthogonal=orthogonal,
+                get_Hamiltonian=get_Hamiltonian,
+                get_overlap=get_overlap,
+                get_DM=get_DM,
+                get_eigenvalues=get_eigenvalues,
+                info_files = info_files
+            )
+
+        else:
+            raise ValueError(f"Not support dataset type: {type}.")
+        
+        return dataset
     
-        for ikey in info_files:
-            info_files[ikey].update({'r_max': r_max, 'er_max': er_max, 'oer_max': oer_max})
-            
-        if dataset_type == "DeePHDataset":
-            dataset = DeePHE3Dataset(
-                root=root,
-                type_mapper=idp,
-                get_Hamiltonian=get_Hamiltonian,
-                get_eigenvalues=get_eigenvalues,
-                info_files = info_files
-            )
-        elif dataset_type == "DefaultDataset":
-            dataset = DefaultDataset(
-                root=root,
-                type_mapper=idp,
-                get_Hamiltonian=get_Hamiltonian,
-                get_overlap=get_overlap,
-                get_DM=get_DM,
-                get_eigenvalues=get_eigenvalues,
-                info_files = info_files
-            )
-        elif dataset_type == "HDF5Dataset":
-            dataset = HDF5Dataset(
-                root=root,
-                type_mapper=idp,
-                get_Hamiltonian=get_Hamiltonian,
-                get_overlap=get_overlap,
-                get_DM=get_DM,
-                get_eigenvalues=get_eigenvalues,
-                info_files = info_files
-            )
-        elif dataset_type == "LMDBDataset":
-            dataset = LMDBDataset(
-            root=root,
-            type_mapper=idp,
-            orthogonal=orthogonal,
-            get_Hamiltonian=get_Hamiltonian,
-            get_overlap=get_overlap,
-            get_DM=get_DM,
-            get_eigenvalues=get_eigenvalues,
-            info_files = info_files
+    def from_model(self, 
+               model, 
+               root: str,
+               type: str = "DefaultDataset",
+               prefix: str = None,
+               separator:str='.',
+               get_Hamiltonian: bool = False,
+               get_overlap: bool = False,
+               get_DM: bool = False,
+               get_eigenvalues: bool = False,
+               # common_options
+               orthogonal: bool = False,
+               basis: str = None, 
+               **kwargs):
+        """
+        Build a dataset from a model.
+
+        Args:
+            - model (torch.nn.Module): The model to build the dataset from.
+            - dataset_type (str, optional): The type of dataset to build. Default is "DefaultDataset".
+
+        Returns:
+            dataset: The built dataset.
+        """
+        cutoff_options = collect_cutoffs(model.model_options)
+    
+        dataset = self(
+            root = root,
+            **cutoff_options,
+            type = type,
+            prefix = prefix,
+            separator = separator,
+            get_Hamiltonian = get_Hamiltonian,
+            get_overlap = get_overlap,
+            get_DM = get_DM,
+            get_eigenvalues = get_eigenvalues,
+            orthogonal = orthogonal,
+            basis = basis, 
+            **kwargs,
         )
 
-    else:
-        raise ValueError(f"Not support dataset type: {type}.")
+        return dataset
 
-    return dataset
+# note, compared to the previous build_dataset, this one is more flexible.    
+# previous build_dataset is a function. now i define a class DataBuilder and re-defined __call__ function.
+# then build_dataset is an instance of DataBuilder class. so i can use build_dataset.from_model() to build dataset from model.
+# at the same time the previous way to use  build_dataset is still available. like build_dataset(...).
+
+build_dataset = DatasetBuilder()
+
