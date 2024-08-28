1dbdbecbdc53303bc2f3b30461b198c74194e5e3
AsymmetryChou <181240085@smail.nju.edu.cn>
2024-08-28 17:42:51 +0800

    add overlap output in feature_to_block

--- dptb/data/interfaces/ham_to_feature.py
+++ dptb/data/interfaces/ham_to_feature.py
@@ -319,16 +319,25 @@ def block_to_feature(data, idp, blocks=False, overlap_blocks=False, orthogonal=F
 #     if overlap_blocks:
 #         data[_keys.EDGE_OVERLAP_KEY] = torch.as_tensor(np.array(edge_overlap), dtype=torch.get_default_dtype())
 
-def feature_to_block(data, idp):
+def feature_to_block(data, idp, mode="H"):
     idp.get_orbital_maps()
     idp.get_orbpair_maps()
 
     has_block = False
-    if data.get(_keys.NODE_FEATURES_KEY, None) is not None:
-        node_features = data[_keys.NODE_FEATURES_KEY]
-        edge_features = data[_keys.EDGE_FEATURES_KEY]
-        has_block = True
-        blocks = {}
+    if mode == "H":
+        if data.get(_keys.NODE_FEATURES_KEY, None) is not None:
+            node_features = data[_keys.NODE_FEATURES_KEY]
+            edge_features = data[_keys.EDGE_FEATURES_KEY]
+            has_block = True
+            blocks = {}
+    elif mode == "S":
+        if data.get(_keys.NODE_OVERLAP_KEY, None) is not None:
+            node_features = data[_keys.NODE_OVERLAP_KEY]
+            edge_features = data[_keys.EDGE_OVERLAP_KEY]
+            has_block = True
+            blocks = {}
+    else:
+        raise ValueError("Mode should be either 'H' or 'S'.")
 
     idp.get_orbital_maps()
     idp.get_orbpair_maps()
