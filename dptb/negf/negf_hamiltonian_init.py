8748b24b8090506848add3b0a084e30df33f6b00
AsymmetryChou <181240085@smail.nju.edu.cn>
2024-08-28 17:41:39 +0800

    remove pbc output

--- dptb/negf/negf_hamiltonian_init.py
+++ dptb/negf/negf_hamiltonian_init.py
@@ -176,9 +176,8 @@ class NEGFHamiltonianInit(object):
         self.structase.set_pbc(self.pbc_negf)
         self.structase.pbc[2] = True
         alldata = AtomicData.from_ase(self.structase, **self.AtomicData_options)
-        print('alldata[AtomicDataDict.PBC_KEY]',alldata[AtomicDataDict.PBC_KEY])
         alldata[AtomicDataDict.PBC_KEY][2] = True # force pbc in z-axis to get reasonable chemical environment in two ends
-        print('alldata[AtomicDataDict.PBC_KEY]',alldata[AtomicDataDict.PBC_KEY])
+
         
         alldata = AtomicData.to_AtomicDataDict(alldata.to(self.torch_device))
         self.alldata = self.model.idp(alldata)
