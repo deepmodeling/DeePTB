5149dea9992e0881312e16b4d19c6b917b687215
qqgu <guqq_phy@qq.com>
2024-10-21 00:12:07 +0800

    update test for dftb2nnsk

--- dptb/tests/test_dftb2nnsk.py
+++ dptb/tests/test_dftb2nnsk.py
@@ -8,46 +8,39 @@ rootdir = os.path.join(Path(os.path.abspath(__file__)).parent, "data/")
 
 class TestDFTB2NNSK:
     rootdir = f"{rootdir}"
-    def test_init(self):
-        dftb2nnsk = DFTB2NNSK(
+    train_ops = {
+        'nstep':10,
+        'dis_freq':1,
+        'nsample':256,
+        'save_freq':1,
+        'max_elmt_batch':4,
+        "optimizer": {
+            "lr": 0.05,
+            "type": "RMSprop"
+        },
+        "lr_scheduler": {
+            "type": "cos",
+            "T_max": 4
+        }
+    }
+    
+    dftb2nnsk = DFTB2NNSK(
             basis={"B":["2s"], "N": ["2s"]}, 
             skdata=os.path.join(rootdir, "slakos"),
+            train_options=train_ops,
             rs=6.0,
             w=1.0,
             functype="powerlaw"
             )
 
     def test_optimize(self):
-        dftb2nnsk = DFTB2NNSK(
-            basis={"B":["2s"], "N": ["2s"]}, 
-            skdata=os.path.join(rootdir, "slakos"),
-            rs=6.0,
-            w=1.0,
-            functype="powerlaw"
-            )
-        dftb2nnsk.optimize(r_min=1,r_max=6,nstep=10)
+        self.dftb2nnsk.optimize(r_min=1,r_max=6,nstep=10)
 
     def test_tonnsk(self):
-        dftb2nnsk = DFTB2NNSK(
-            basis={"B":["2s"], "N": ["2s"]}, 
-            skdata=os.path.join(rootdir, "slakos"),
-            rs=6.0,
-            w=1.0,
-            functype="powerlaw"
-            )
-        nnsk = dftb2nnsk.to_nnsk()
-
+        nnsk = self.dftb2nnsk.to_nnsk()
         assert isinstance(nnsk, NNSK)
         
-
     def test_tojson(self):
-        dftb2nnsk = DFTB2NNSK(
-            basis={"B":["2s"], "N": ["2s"]}, 
-            skdata=os.path.join(rootdir, "slakos"),
-            rs=6.0,
-            w=1.0,
-            functype="powerlaw"
-            )
-        jdata = dftb2nnsk.to_json()
+        jdata = self.dftb2nnsk.to_json()
         assert isinstance(jdata, dict)
         
