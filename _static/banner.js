779eedcb1df42832c8dc382efa99f10cda7903de
QG-phy <QG-phy@users.noreply.github.com>
2026-01-25 05:00:38 +0000

    deploy: b725383bc8daf943fbffe0538187ce7e5082ad6c

--- _static/banner.js
+++ _static/banner.js
@@ -1,2 +1,57 @@
-window.mobileMenu={bind:function(){document.querySelectorAll("[data-behavior='open-mobile-menu']").forEach(function(element){element.addEventListener("click",function(e){e.preventDefault();document.querySelector(".mobile-main-menu").classList.add("open");document.body.classList.add("no-scroll");mobileMenu.listenForResize();});});document.querySelectorAll("[data-behavior='close-mobile-menu']").forEach(function(element){element.addEventListener("click",function(e){e.preventDefault();mobileMenu.close();});});},listenForResize:function(){function resizeHandler(){if(window.innerWidth>768){mobileMenu.close();}}
-window.addEventListener("resize",resizeHandler);this.resizeHandler=resizeHandler;},close:function(){document.querySelector(".mobile-main-menu").classList.remove("open");document.body.classList.remove("no-scroll");if(this.resizeHandler){window.removeEventListener("resize",this.resizeHandler);this.resizeHandler=null;}},};document.addEventListener("DOMContentLoaded",function(){mobileMenu.bind();});
+/* The initial version is taken from
+https://github.com/pytorch/pytorch_sphinx_theme
+under MIT license
+rewrite to remove jQuery by ChatGPT
+*/
+window.mobileMenu = {
+  bind: function () {
+    document
+      .querySelectorAll("[data-behavior='open-mobile-menu']")
+      .forEach(function (element) {
+        element.addEventListener("click", function (e) {
+          e.preventDefault();
+          document.querySelector(".mobile-main-menu").classList.add("open");
+          document.body.classList.add("no-scroll");
+
+          mobileMenu.listenForResize();
+        });
+      });
+
+    document
+      .querySelectorAll("[data-behavior='close-mobile-menu']")
+      .forEach(function (element) {
+        element.addEventListener("click", function (e) {
+          e.preventDefault();
+          mobileMenu.close();
+        });
+      });
+  },
+
+  listenForResize: function () {
+    function resizeHandler() {
+      if (window.innerWidth > 768) {
+        mobileMenu.close();
+      }
+    }
+
+    window.addEventListener("resize", resizeHandler);
+
+    // Store the handler so it can be removed later
+    this.resizeHandler = resizeHandler;
+  },
+
+  close: function () {
+    document.querySelector(".mobile-main-menu").classList.remove("open");
+    document.body.classList.remove("no-scroll");
+
+    // Remove the resize event listener
+    if (this.resizeHandler) {
+      window.removeEventListener("resize", this.resizeHandler);
+      this.resizeHandler = null;
+    }
+  },
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  mobileMenu.bind();
+});
